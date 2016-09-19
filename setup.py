30d7a03cbe00dada48e52f1bb5871c22ad9ebb22
ssomnath <somnaths@ornl.gov>
2016-09-19 13:36:36 -0400

    Updated setup.py Corrected scikit learn package name

--- setup.py
+++ setup.py
@@ -34,14 +34,14 @@ setup(
     author_email='pycroscopy@gmail.com',
 
     # I don't remember how to do this correctly!!!. NL
-    dependency='',
-    dependency_links=[''],
     install_requires=['numpy_groupies','pyqtgraph','numpy>=1.11.0',
                       'h5py>=2.6.0','scipy>=0.17.1','matplotlib>=1.5.1',
-                      'sklearn>=0.17.1','xlrd>=1.0.0'],#'multiprocess'],
+                      'scikit-learn>=0.17.1','xlrd>=1.0.0'],#'multiprocess'],
     #package_data={'sample':['dataset_1.dat']}
     test_suite='nose.collector',
     tests_require='Nose',
+    dependency='',
+    dependency_links=[''],
     include_package_data=True,
 
     # If there are data files included in your packages that need to be
