341f3b27be43fec50bd22e49962ed3c7bc20803e
Chris Smith <csmith55@utk.edu>
2016-09-20 10:46:26 -0400

    Import fixes

--- pycroscopy/io/translators/be_odf_relaxation.py
+++ pycroscopy/io/translators/be_odf_relaxation.py
@@ -15,7 +15,7 @@ from .utils import makePositionMat, getPositionSlicing, generateDummyMainParms
 from .be_utils import trimUDVS, getSpectroscopicParmLabel, generatePlotGroups, createSpecVals
 from ..microdata import MicroDataGroup, MicroDataset # The building blocks for defining heirarchical storage in the H5 file
 from ..io_hdf5 import ioHDF5 # Now the translator is responsible for writing the data.
-from ..beutils import maxReadPixels
+from ..be_hdf_utils import maxReadPixels
 from ..hdf_utils import getH5DsetRefs
 
 class BEodfRelaxationTranslator(Translator):
