c254bf20bc8b4b7c73e3361d3666fb3733dbc09f
Chris Smith <csmith55@utk.edu>
2016-09-21 16:32:06 -0400

    Revert "Commented out unimplemented imports"
    This reverts commit f6b76db8f963d28c0a9f2875139d5e286e3bd01b.

--- pycroscopy/processing/__init__.py
+++ pycroscopy/processing/__init__.py
@@ -8,17 +8,17 @@ def no_impl(*args,**kwargs):
     raise NotImplementedError("You need to install Multiprocess package (pip,github) to do a parallel Computation.\n"
                               "Switching to the serial version. ")
 
-# from .feature_extraction import FeatureExtractorParallel, FeatureExtractorSerial
-# from .geometric_transformation import geoTransformerParallel, geoTransformerSerial
-# 
-# FeatureExtractor = FeatureExtractorSerial
-# geoTransformer = geoTransformerSerial
-# 
-# try:
-#     import multiprocess
-# except ImportError:
-#     FeatureExtractorParallel = no_impl
-#     geoTransformerParallel = no_impl
-# else:
-#     FeatureExtractor = FeatureExtractorParallel
-#     geoTransformer = geoTransformerParallel
+from .feature_extraction import FeatureExtractorParallel, FeatureExtractorSerial
+from .geometric_transformation import geoTransformerParallel, geoTransformerSerial
+
+FeatureExtractor = FeatureExtractorSerial
+geoTransformer = geoTransformerSerial
+
+try:
+    import multiprocess
+except ImportError:
+    FeatureExtractorParallel = no_impl
+    geoTransformerParallel = no_impl
+else:
+    FeatureExtractor = FeatureExtractorParallel
+    geoTransformer = geoTransformerParallel
