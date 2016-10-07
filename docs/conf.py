7797680f8e8b5e582affc16a7a775d30439fe8b5
Chris Smith <csmith55@utk.edu>
2016-10-07 12:25:04 -0400

    Add 'mock' modules for packages that require C code
    Added mock modules for numpy, scipy, matplotlib, and PyQt4 to docs\conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+import sys
+import mock
 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.misc', 'PyQt4']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
