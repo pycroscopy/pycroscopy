555a81fce371080c24c5ccd595438963353826f9
Chris Smith <csmith55@utk.edu>
2016-10-07 11:24:39 -0400

    Removed import of Model
    Import of Model.py in analysis/__init__.py was removed until errors in the Class are fixed.

--- pycroscopy/analysis/__init__.py
+++ pycroscopy/analysis/__init__.py
@@ -1,9 +1,7 @@
-from . import Model
-from .Model import Model
 from . import guess_methods
 from .guess_methods import GuessMethods
 from . import utils
 from .utils import *
 
-__all__ = ['Model', 'GuessMethods', 'utils']
+__all__ = ['GuessMethods', 'utils']
 __all__ += utils.__all__
