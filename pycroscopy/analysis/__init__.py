254da297788c6b180fb91f42067fe3b064a863a9
Chris Smith <csmith55@utk.edu>
2016-10-10 09:18:47 -0400

    Added model import back to analysis/__init__.py

--- pycroscopy/analysis/__init__.py
+++ pycroscopy/analysis/__init__.py
@@ -1,8 +1,11 @@
 from . import utils
-from .be_sho_model import BESHOmodel
-from .guess_methods import GuessMethods
-from .model import Model
 from .utils import *
+from . import be_sho_model
+from .be_sho_model import BESHOmodel
+from . import guess_methods
+from .guess_methods import GuessMethods
+from . import model
+from .model import Model
 
 __all__ = ['GuessMethods', 'Model', 'BESHOmodel','utils']
 __all__ += utils.__all__
