49626ff634541846d5ae9bcc45a8e646980fd0e5
Chris Smith <csmith55@utk.edu>
2016-10-12 09:46:31 -0400

    More mocks

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
                 'mpl_toolkits.axes_grid1', 'scipy.optimize',
                 'scipy.spatial', 'scipy.signal', 'scipy.weave',
                 'scipy.io.matlab', 'sklearn', 'sklearn.utils',
+                'sklearn.cluster', 'sklearn.decomposition',
                 'sklearn.utils.extmath']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
