2784180915cd40aee010ccaafc155cb0daa67cc9
Chris Smith <csmith55@utk.edu>
2017-01-05 10:46:20 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='pycroscopy',
-    version='0.0a18',
+    version='0.0a19',
     description='A suite of Python libraries for high performance scientific computing of microscopy data.',
     long_description=long_description,
     classifiers=[
