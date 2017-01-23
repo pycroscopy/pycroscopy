00ed4f97c5bf35de662210d0c4e4a0163d700897
Chris Smith <csmith55@utk.edu>
2017-01-23 09:51:15 -0500

    Fix to create_empty_dataset
    Added parameter skip_refs to call signature, defaults to True
    Pass skip_refs parameter to copyAttributes

--- pycroscopy/io/hdf_utils.py
+++ pycroscopy/io/hdf_utils.py
@@ -724,7 +724,7 @@ def get_sort_order(ds_spec):
     return change_sort
 
 
-def create_empty_dataset(source_dset, dtype, dset_name, ds_attrs=dict()):
+def create_empty_dataset(source_dset, dtype, dset_name, ds_attrs=dict(), skip_refs=False):
     """
     Creates an empty dataset in the h5 file based in the same group as the provided dataset
 
@@ -738,6 +738,9 @@ def create_empty_dataset(source_dset, dtype, dset_name, ds_attrs=dict()):
         Name of the dataset
     ds_attrs : dictionary (Optional)
         Any new attributes that need to be written to the dataset
+    skip_refs : boolean, optional
+        Should ObjectReferences and RegionReferences be skipped when copying attributes from the
+        `source_dset`
 
     Returns
     -------
@@ -747,7 +750,7 @@ def create_empty_dataset(source_dset, dtype, dset_name, ds_attrs=dict()):
     h5_group = source_dset.parent
     h5_new_dset = h5_group.create_dataset(dset_name, shape=source_dset.shape, dtype=dtype)
     # This should link the ancillary datasets correctly
-    h5_new_dset = copyAttributes(source_dset, h5_new_dset)
+    h5_new_dset = copyAttributes(source_dset, h5_new_dset, skip_refs=skip_refs)
     h5_new_dset.attrs.update(ds_attrs)
 
     return h5_new_dset
