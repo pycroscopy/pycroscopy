c14029d877ba503ad6a83dce896babd0cc95276d
Chris Smith <csmith55@utk.edu>
2017-03-27 11:59:25 -0400

    Version increment

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
     name='pycroscopy',
-    version='0.0a34',
+    version='0.0a35',
     description='A suite of Python libraries for high performance scientific computing of microscopy data.',
     long_description=long_description,
     classifiers=[
