e8c3f7f65daba40241d56bdcaedf7ad78d928881
Unknown <ondrej.dyck@gmail.com>
2017-04-07 18:16:49 -0400

    Friday April 7

--- pycroscopy/analysis/utils/atom_finding_general_gaussian.py
+++ pycroscopy/analysis/utils/atom_finding_general_gaussian.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-
+import os
 import numpy as np
 from scipy.optimize import least_squares
 import itertools as itt
@@ -8,6 +8,7 @@ import multiprocessing as mp
 import time as tm
 from _warnings import warn
 from sklearn.neighbors import KNeighborsClassifier
+import h5py
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -20,7 +21,7 @@ from pycroscopy.io.microdata import MicroDataset, MicroDataGroup
 from pycroscopy.io.io_hdf5 import ioHDF5
 from pycroscopy.analysis.model import Model
 
-class Gauss_Fit(Model):#, fitting_parms=None, num_cores=None):
+class Gauss_Fit(object):#, fitting_parms=None, num_cores=None):
         """
         Computes the guess and fit coefficients for the provided atom guess positions and writes these results to the
         given h5 group
@@ -41,22 +42,24 @@ class Gauss_Fit(Model):#, fitting_parms=None, num_cores=None):
             Same group as the parameter but now with the 'Guess' and 'Fit' datasets
         """
 
-        def __init__(self, h5_main, variables=['Frequency'], parallel=True):
-            super(Gauss_Fit, self).__init__(h5_main, variables, parallel)
+        def __init__(self, h5_main, variables, parallel=True):
+            print('Initializing Gauss Fit')
+            print(h5_main)
+            #super(Gauss_Fit, self).__init__(h5_main, variables, parallel)
 
 
 
 
-            pass
+
 
 if __name__=='__main__':
-    h5_path = 'C:\Users\o2d\Desktop\MouseWithoutBorders\Atom position refinement testing\Simulated_image\image 04.h5'
-    folder_path, file_path = os.path.split(h5_path)
+    file_name = 'C:\Users\o2d\Desktop\MouseWithoutBorders\Atom position refinement testing\Simulated_image\image 04.h5'
+    folder_path, file_path = os.path.split(file_name)
 
-    print(image_path)
 
     file_base_name, file_extension = file_name.rsplit('.')
-    h5_file = h5py.File(image_path, mode='r+')
+    h5_file = h5py.File(file_name, mode='r+')
+
 
 
     # look at the data tree in the h5
@@ -76,3 +79,11 @@ if __name__=='__main__':
     print('Datasets and datagroups within the file:')
     file_handle = h5_file
     print_tree(file_handle)
+
+    cropped_clean_image = h5_file['/Measurement_000/Channel_000/Raw_Data-Windowing_000/Image_Windows-SVD_000/U-Cluster_000/Labels-Atom_Finding_000/Cropped_Clean_Image']
+    atom_finding = h5_file['/Measurement_000/Channel_000/Raw_Data-Windowing_000/Image_Windows-SVD_000/U-Cluster_000/Labels-Atom_Finding_000']
+    atom_grp = cropped_clean_image.parent
+    guess_params = atom_grp['Guess_Positions']
+
+    foo = Gauss_Fit(atom_grp, variables=['Cropped_Clean_Image', 'Guess_Positions'])
+
