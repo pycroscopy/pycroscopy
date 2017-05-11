697f51f2217ad2a61902e1fde611d152ab95df6e
Unknown <csmith55@utk.edu>
2017-05-11 10:09:02 -0400

    Rest of changes that should have been in GMode cleanup

--- pycroscopy/processing/gmode_utils.py
+++ pycroscopy/processing/gmode_utils.py
@@ -79,28 +79,30 @@ def test_filter(resp_wfm, filter_parms, samp_rate, show_plots=True, use_rainbow_
     
     show_loops = excit_wfm is not None and show_plots
     
-    noise_band_filter = 1
-    low_pass_filter = 1
-    harmonic_filter = 1
-    noise_floor = None
+    '''
+    Get parameters from the dictionary.
+    '''
+    noise_band_filter = filter_parms.pop('band_file_[Hz]', 1)
+    if type(filter_parms['band_filt_[Hz]']) in [list, np.ndarray]:
+        noise_band_filter = noiseBandFilter(num_pts, samp_rate, noise_band_filter[0],
+                                            noise_band_filter[1])
 
-    if 'band_filt_[Hz]' in filter_parms:
-        if type(filter_parms['band_filt_[Hz]']) in [list, np.ndarray]:
-            noise_band_filter = noiseBandFilter(num_pts, samp_rate, filter_parms['band_filt_[Hz]'][0],
-                                                filter_parms['band_filt_[Hz]'][1])
-    if 'LPF_cutOff_[Hz]' in filter_parms:
-        if filter_parms['LPF_cutOff_[Hz]'] > 0:
-            low_pass_filter = makeLPF(num_pts, samp_rate, filter_parms['LPF_cutOff_[Hz]'])
+    low_pass_filter = filter_parms.pop('LPF_cutOff_[Hz]', 1)
+    if filter_parms['LPF_cutOff_[Hz]'] > 0:
+        low_pass_filter = makeLPF(num_pts, samp_rate, low_pass_filter)
 
-    if 'comb_[Hz]' in filter_parms:
-        if type(filter_parms['comb_[Hz]']) in [list, np.ndarray]:
-            harmonic_filter = harmonicsPassFilter(num_pts, samp_rate, filter_parms['comb_[Hz]'][0],
-                                                  filter_parms['comb_[Hz]'][1], filter_parms['comb_[Hz]'][2])
+    harmonic_filter = filter_parms.pop('comb_[Hz]', 1)
+    if type(filter_parms['comb_[Hz]']) in [list, np.ndarray]:
+        harmonic_filter = harmonicsPassFilter(num_pts, samp_rate, harmonic_filter[0],
+                                              harmonic_filter[1], harmonic_filter[2])
 
     composite_filter = noise_band_filter * low_pass_filter * harmonic_filter
-        
+
+    noise_floor = filter_parms.pop('noise_threshold', None)
     fft_pix_data = np.fft.fftshift(np.fft.fft(resp_wfm))
-    
+    if 0 < noise_floor < 1:
+        noise_floor = getNoiseFloor(fft_pix_data, noise_floor)[0]
+
     if show_plots:       
         l_ind = int(0.5*num_pts)
         r_ind = max(np.where(composite_filter > 0)[0])
@@ -126,10 +128,6 @@ def test_filter(resp_wfm, filter_parms, samp_rate, show_plots=True, use_rainbow_
         fig = None
         axes = None
 
-    if 'noise_threshold' in filter_parms:
-        if filter_parms['noise_threshold'] > 0 and filter_parms['noise_threshold'] < 1:
-            noise_floor = getNoiseFloor(fft_pix_data, filter_parms['noise_threshold'])[0]
-
     if show_plots:
         amp = np.abs(fft_pix_data)
         ax_raw.semilogy(w_vec[l_ind:r_ind], amp[l_ind:r_ind])
@@ -148,7 +146,6 @@ def test_filter(resp_wfm, filter_parms, samp_rate, show_plots=True, use_rainbow_
         ax_filt.set_title('Filtered Signal')
         ax_filt.set_xlabel('Frequency(kHz)')
         if noise_floor is not None:
-            orig_lims = ax_raw.get_ylim()
             ax_filt.set_ylim(bottom=noise_floor)  # prevents the noise threshold from messing up plots
 
     filt_data = np.real(np.fft.ifft(np.fft.ifftshift(fft_pix_data)))
