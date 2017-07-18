5075bd9060a19bf3c4ce3fec034e7067575da21a
Unknown <csmith55@utk.edu>
2017-07-18 09:56:44 -0400

    Docstrings for Fake BEPS and bugfixes
    Added docstrings to FakeBEPS functions
    Write input parameters to measurement group, not channel group

--- pycroscopy/io/translators/df_utils/beps_gen_utils.py
+++ pycroscopy/io/translators/df_utils/beps_gen_utils.py
@@ -7,14 +7,19 @@ beps_image_folder = os.path.abspath(os.path.join(os.path.realpath(__file__), '..
 
 def combine_in_out_field_loops(in_vec, out_vec):
     """
+    Stack the in-field and out-of-field loops
 
     Parameters
     ----------
-    in_vec
-    out_vec
+    in_vec : numpy.ndarray
+        1d array of in-field values
+    out_vec : numpy.ndarray
+        1d array of out-of-field values
 
     Returns
     -------
+    field_mat : numpy.ndarray
+        2d array of combined in-field and out-of-field vectors
 
     """
     return np.vstack((in_vec, out_vec))
@@ -22,6 +27,7 @@ def combine_in_out_field_loops(in_vec, out_vec):
 
 def build_loop_from_mat(loop_mat, num_steps):
     """
+
 
     Parameters
     ----------
@@ -39,14 +45,19 @@ def build_loop_from_mat(loop_mat, num_steps):
 
 def get_noise_vec(num_pts, noise_coeff):
     """
+    Calculate a multiplicative noise vector from the `noise_coeff`
 
     Parameters
     ----------
-    num_pts
-    noise_coeff
+    num_pts : uint
+        number of points in the vector
+    noise_coeff : float
+        Noise coefficient that determines the variation in the noise vector
 
     Returns
     -------
+    noise_vec : numpy.ndarray
+        1d noise vector array
 
     """
     return np.ones(num_pts) * (1 + 0.5 * noise_coeff) - np.random.random(num_pts) * noise_coeff
