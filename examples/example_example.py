ff077e7d8295229aedb33a964026e73acc98dc66
Chris Smith <csmith55@utk.edu>
2017-07-27 12:12:20 -0400

    Sample examples
    Two simple example scripts to use as guidelines for creating more.

---
+++ examples/example_example.py
@@ -0,0 +1,14 @@
+
+"""
+An Example on writing examples
+==============================
+
+The example docstring will be printed as text along with the code of the example.
+It should contain the desciption of the example code.
+Only examples that begine with plot_* will be run when generating the docs.
+
+"""
+
+# Code source: Chris Smith
+# Liscense: MIT
+print('I am an example')
