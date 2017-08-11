39ff5b18f22c659f33b9b2d9d6f64065530d4d0a
syz <somnaths@ornl.gov>
2017-08-11 13:14:36 -0400

    All interactive widgets handle rectangular spatial maps correctly now and accept a cmap

--- pycroscopy/viz/be_viz_utils.py
+++ pycroscopy/viz/be_viz_utils.py
@@ -12,12 +12,12 @@ from matplotlib import pyplot as plt
 import ipywidgets as widgets
 from IPython.display import display
 
-from .plot_utils import plot_loops, plot_map_stack, cmap_jet_white_center, plot_map
+from .plot_utils import plot_loops, plot_map_stack, plot_map, get_cmap_object
 from ..io.hdf_utils import reshape_to_Ndims, getAuxData, get_sort_order, get_dimensionality, get_attr
 from ..analysis.utils.be_loop import loop_fit_function
 
 
-def visualize_sho_results(h5_main, save_plots=True, show_plots=True):
+def visualize_sho_results(h5_main, save_plots=True, show_plots=True, cmap=None):
     """
     Plots some loops, amplitude, phase maps for BE-Line and BEPS datasets.\n
     Note: The file MUST contain SHO fit gusses at the very least
@@ -30,11 +30,14 @@ def visualize_sho_results(h5_main, save_plots=True, show_plots=True):
         Whether or not to save plots to files in the same directory as the h5 file
     show_plots : (Optional) Boolean
         Whether or not to display the plots on the screen
+    cmap : String, or matplotlib.colors.LinearSegmentedColormap object (Optional)
+        Requested color map
 
     Returns
     -------
     None
     """
+    cmap = get_cmap_object(cmap)
 
     def __plot_loops_maps(ac_vec, resp_mat, grp_name, win_title, spec_var_title, meas_var_title, save_plots,
                           folder_path, basename, num_rows, num_cols):
@@ -47,7 +50,7 @@ def visualize_sho_results(h5_main, save_plots=True, show_plots=True):
         plt_title = grp_name + '_' + win_title + '_Snaps'
         fig, axes = plot_map_stack(resp_mat.reshape(num_rows, num_cols, resp_mat.shape[1]),
                                    color_bar_mode="each", evenly_spaced=True, title='UDVS Step #',
-                                   heading=plt_title, cmap=plt.cm.viridis)
+                                   heading=plt_title, cmap=cmap)
         if save_plots:
             fig.savefig(os.path.join(folder_path, basename + '_' + plt_title + '.png'), format='png', dpi=300)
 
@@ -148,7 +151,7 @@ def visualize_sho_results(h5_main, save_plots=True, show_plots=True):
         fig_ms, ax_ms = plot_map_stack(np.dstack((amp_mat, freq_mat, q_mat, phase_mat, rsqr_mat)),
                                        num_comps=5, color_bar_mode='each', heading=grp_name,
                                        title=['Amplitude (mV)', 'Frequency (kHz)', 'Quality Factor', 'Phase (deg)',
-                                              'R^2 Criterion'], cmap=plt.cm.viridis)
+                                              'R^2 Criterion'], cmap=cmap)
 
         if save_plots:
             plt_path = os.path.join(folder_path, basename + '_' + grp_name + 'Maps.png')
@@ -205,7 +208,7 @@ def plot_loop_guess_fit(vdc, ds_proj_loops, ds_guess, ds_fit, title=''):
     return fig, axes
 
 
-def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_label='Response'):
+def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_label='Response', cmap=None):
     """
     Jupyer notebook ONLY function. Sets up an interactive visualizer for viewing SHO fitted BEPS data.
     Currently, this is limited to DC and AC spectroscopy datasets.
@@ -221,7 +224,11 @@ def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_labe
         uses A
     resp_label : string / unicode (optional)
         Label for the response (y) axis.
+    cmap : String, or matplotlib.colors.LinearSegmentedColormap object (Optional)
+        Requested color map
     """
+    cmap = get_cmap_object(cmap)
+
     guess_3d_data, success = reshape_to_Ndims(h5_sho_dset)
 
     h5_sho_spec_inds = getAuxData(h5_sho_dset, 'Spectroscopic_Indices')[0]
@@ -300,7 +307,7 @@ def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_labe
     ax_bias.set_ylabel(step_chan.replace('_', ' ') + ' (V)')
     bias_slider = ax_bias.axvline(x=step_ind, color='r')
 
-    img_map = ax_map.imshow(spatial_map, cmap=plt.cm.viridis, origin='lower',
+    img_map = ax_map.imshow(spatial_map.T, cmap=cmap, origin='lower',
                             interpolation='none')
     ax_map.set_xlabel('X')
     ax_map.set_ylabel('Y')
@@ -318,7 +325,7 @@ def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_labe
         step_ind = kwargs['Bias Step']
         bias_slider.set_xdata((step_ind, step_ind))
         spatial_map = sho_dset_collapsed[:, :, step_ind, 0][sho_quantity]
-        img_map.set_data(spatial_map)
+        img_map.set_data(spatial_map.T)
         spat_mean = np.mean(spatial_map)
         spat_std = np.std(spatial_map)
         img_map.set_clim(vmin=spat_mean - 3 * spat_std, vmax=spat_mean + 3 * spat_std)
@@ -328,7 +335,7 @@ def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_labe
         main_vert_line.set_xdata((kwargs['X'], kwargs['X']))
         main_hor_line.set_ydata((kwargs['Y'], kwargs['Y']))
 
-        resp_vec = sho_dset_collapsed[row_ind, col_ind, :, :]
+        resp_vec = sho_dset_collapsed[col_ind, row_ind, :, :]
         resp_vec = resp_func(resp_vec)
         for line_handle, data in zip(line_handles, np.transpose(resp_vec)):
             line_handle.set_ydata(data)
@@ -343,7 +350,7 @@ def jupyter_visualize_beps_sho(h5_sho_dset, step_chan, resp_func=None, resp_labe
     widgets.interact(update_sho_plots, sho_quantity=list(sho_dset_collapsed.dtype.names[:-1]), **slider_dict)
 
 
-def jupyter_visualize_be_spectrograms(h5_main):
+def jupyter_visualize_be_spectrograms(h5_main, cmap=None):
     """
     Jupyer notebook ONLY function. Sets up a simple visualzier for visualizing raw BE data.
     Sliders for position indices can be used to visualize BE spectrograms (frequency, UDVS step).
@@ -352,9 +359,12 @@ def jupyter_visualize_be_spectrograms(h5_main):
     Parameters
     ----------
     h5_main : h5py.Dataset
-        Raw dataset
+        Raw Band Excitation dataset
+    cmap : String, or matplotlib.colors.LinearSegmentedColormap object (Optional)
+        Requested color map
     """
-    # TODO add colormap options
+    cmap=get_cmap_object(cmap)
+
     h5_pos_inds = getAuxData(h5_main, auxDataName='Position_Indices')[-1]
     pos_sort = get_sort_order(np.transpose(h5_pos_inds))
     pos_dims = get_dimensionality(np.transpose(h5_pos_inds), pos_sort)
@@ -372,15 +382,16 @@ def jupyter_visualize_be_spectrograms(h5_main):
     try:
         num_udvs_steps = h5_main.parent.parent.attrs['num_udvs_steps']
     except KeyError:
-        num_udvs_steps = h5_main.parent.parent.attrs['num_UDVS_steps']
+        num_udvs_steps = 1
 
     if len(pos_dims) == 2:
-        spatial_map = np.abs(np.reshape(h5_main[:, 0], pos_dims))
+        spatial_map = np.abs(np.reshape(h5_main[:, 0], pos_dims[::-1]))
         spectrogram = np.reshape(h5_main[0], (num_udvs_steps, -1))
         fig, axes = plt.subplots(ncols=3, figsize=(12, 4))
         # spatial_img = axes[0].imshow(np.abs(spatial_map), cmap=plt.cm.viridis)
         spatial_img = plot_map(axes[0], np.abs(spatial_map), origin='lower',
-                               cmap=plt.cm.viridis)
+                               cmap=cmap)
+        axes[0].set_aspect(1)
         axes[0].set_xlabel('X')
         axes[0].set_ylabel('Y')
         main_vert_line = axes[0].axvline(x=int(0.5 * spatial_map.shape[1]), color='k')
@@ -388,13 +399,13 @@ def jupyter_visualize_be_spectrograms(h5_main):
 
         if len(spec_dims) > 1:
             # BEPS
-            amp_img = axes[1].imshow(np.abs(spectrogram), cmap=plt.cm.viridis,
+            amp_img = axes[1].imshow(np.abs(spectrogram), cmap=cmap,
                                      extent=[freqs_2d[0, 0], freqs_2d[-1, 0],
                                              spectrogram.shape[0], 0],
                                      interpolation='none')
             axes[1].set_ylabel('BE step')
 
-            phase_img = axes[2].imshow(np.angle(spectrogram), cmap=plt.cm.viridis,
+            phase_img = axes[2].imshow(np.angle(spectrogram), cmap=cmap,
                                        extent=[freqs_2d[0, 0], freqs_2d[-1, 0],
                                                spectrogram.shape[0], 0],
                                        interpolation='none')
@@ -425,7 +436,7 @@ def jupyter_visualize_be_spectrograms(h5_main):
         fig.tight_layout()
 
         def index_unpacker(**kwargs):
-            spatial_map = np.abs(np.reshape(h5_main[:, kwargs['spectroscopic']], pos_dims))
+            spatial_map = np.abs(np.reshape(h5_main[:, kwargs['spectroscopic']], pos_dims[::-1]))
             spatial_img.set_data(spatial_map)
             spat_mean = np.mean(spatial_map)
             spat_std = np.std(spatial_map)
@@ -466,13 +477,13 @@ def jupyter_visualize_be_spectrograms(h5_main):
         def plot_spectrogram(data, freq_vals):
             fig, axes = plt.subplots(ncols=2, figsize=(9, 5), sharey=True)
             im_handles = list()
-            im_handles.append(axes[0].imshow(np.abs(data), cmap=plt.cm.viridis,
+            im_handles.append(axes[0].imshow(np.abs(data), cmap=cmap,
                                              extent=[freqs_2d[0, 0], freqs_2d[-1, 0],
                                                      data.shape[0], 0],
                                              interpolation='none'))
             axes[0].set_title('Amplitude')
             axes[0].set_ylabel('BE step')
-            im_handles.append(axes[1].imshow(np.angle(data), cmap=plt.cm.viridis,
+            im_handles.append(axes[1].imshow(np.angle(data), cmap=cmap,
                                              extent=[freqs_2d[0, 0], freqs_2d[-1, 0],
                                                      data.shape[0], 0],
                                              interpolation='none'))
@@ -506,10 +517,10 @@ def jupyter_visualize_be_spectrograms(h5_main):
         widgets.interact(position_unpacker, **pos_dict)
 
 
-def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit, step_chan='DC_Offset'):
+def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit, step_chan='DC_Offset', cmap=None):
     """
     Interactive plotting of the BE Loops
-    
+
     Parameters
     ----------
     h5_projected_loops : h5py.Dataset
@@ -521,12 +532,15 @@ def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit,
     step_chan : str, optional
         The name of the Spectroscopic dimension to plot versus.  Needs testing.
         Default 'DC_Offset'
+    cmap : String, or matplotlib.colors.LinearSegmentedColormap object (Optional)
+        Requested color map
 
     Returns
     -------
     None
-    
     """
+    cmap = get_cmap_object(cmap)
+
     # Prepare some variables for plotting loops fits and guesses
     # Plot the Loop Guess and Fit Results
     proj_nd, _ = reshape_to_Ndims(h5_projected_loops)
@@ -535,9 +549,9 @@ def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit,
 
     h5_projected_loops = h5_loop_guess.parent['Projected_Loops']
     h5_proj_spec_inds = getAuxData(h5_projected_loops,
-                                                auxDataName='Spectroscopic_Indices')[-1]
+                                   auxDataName='Spectroscopic_Indices')[-1]
     h5_proj_spec_vals = getAuxData(h5_projected_loops,
-                                                auxDataName='Spectroscopic_Values')[-1]
+                                   auxDataName='Spectroscopic_Values')[-1]
     h5_pos_inds = getAuxData(h5_projected_loops,
                              auxDataName='Position_Indices')[-1]
     pos_nd, _ = reshape_to_Ndims(h5_pos_inds, h5_pos=h5_pos_inds)
@@ -554,25 +568,25 @@ def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit,
     # # move the step dimension to be the first after all position dimensions
     rest_loop_dim_order = list(range(len(pos_dims), len(proj_nd.shape)))
     rest_loop_dim_order.pop(spec_step_dim_ind)
-    new_order = list(range(len(pos_dims))) + [len(pos_dims)+spec_step_dim_ind] + rest_loop_dim_order
+    new_order = list(range(len(pos_dims))) + [len(pos_dims) + spec_step_dim_ind] + rest_loop_dim_order
 
-    new_spec_order = np.array(new_order[len(pos_dims):], dtype=np.uint32)-len(pos_dims)
+    new_spec_order = np.array(new_order[len(pos_dims):], dtype=np.uint32) - len(pos_dims)
     # new_spec_dims = loop_spec_dims[new_spec_order]
     # new_spec_labels = loop_spec_labels[new_spec_order]
 
-    #Also reshape the projected loops to Positions-DC_Step-Loop
-    final_loop_shape = pos_dims+[loop_spec_dims[spec_step_dim_ind]]+[-1]
+    # Also reshape the projected loops to Positions-DC_Step-Loop
+    final_loop_shape = pos_dims + [loop_spec_dims[spec_step_dim_ind]] + [-1]
     proj_nd_3 = np.reshape(proj_nd, final_loop_shape)
 
     # Do the same for the guess and fit datasets
-    guess_3d = np.reshape(guess_nd, pos_dims+[-1])
+    guess_3d = np.reshape(guess_nd, pos_dims + [-1])
     fit_3d = np.reshape(fit_nd, pos_dims + [-1])
 
     # Get the bias vector:
     bias_vec = np.reshape(spec_nd[spec_step_dim_ind], final_loop_shape[len(pos_dims):])
 
     # Shift the bias vector and the loops by a quarter cycle
-    shift_ind = int(-1*bias_vec.shape[0]/4)
+    shift_ind = int(-1 * bias_vec.shape[0] / 4)
     bias_shifted = np.roll(bias_vec, shift_ind, axis=0)
     proj_nd_shifted = np.roll(proj_nd_3, shift_ind, axis=len(pos_dims))
 
@@ -584,22 +598,22 @@ def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit,
     col_ind = 0
 
     # Initial plot data
-    spatial_map = fit_3d[:,:, loop_ind][loop_field]
-    proj_data = proj_nd_shifted[row_ind, col_ind, :, loop_ind]
+    spatial_map = fit_3d[:, :, loop_ind][loop_field]
+    proj_data = proj_nd_shifted[col_ind, row_ind, :, loop_ind]
     bias_data = bias_shifted[:, loop_ind]
-    guess_data = loop_fit_function(bias_data, np.array(list(guess_3d[row_ind, col_ind, loop_ind])))
-    fit_data = loop_fit_function(bias_data, np.array(list(fit_3d[row_ind, col_ind, loop_ind])))
+    guess_data = loop_fit_function(bias_data, np.array(list(guess_3d[col_ind, row_ind, loop_ind])))
+    fit_data = loop_fit_function(bias_data, np.array(list(fit_3d[col_ind, row_ind, loop_ind])))
 
     fig = plt.figure(figsize=(12, 8))
     ax_map = plt.subplot2grid((1, 2), (0, 0), colspan=1, rowspan=1)
     ax_loop = plt.subplot2grid((1, 2), (0, 1), colspan=1, rowspan=1)
 
-    im_map = ax_map.imshow(spatial_map, cmap=plt.cm.viridis,
+    im_map = ax_map.imshow(spatial_map.T, cmap=cmap,
                            origin='lower', interpolation='none')
     ax_map.set_xlabel('X')
     ax_map.set_ylabel('Y')
-    main_vert_line = ax_map.axvline(x=row_ind, color='k')
-    main_hor_line = ax_map.axhline(y=col_ind, color='k')
+    main_vert_line = ax_map.axvline(x=col_ind, color='k')
+    main_hor_line = ax_map.axhline(y=row_ind, color='k')
 
     ax_loop.plot(bias_data, proj_data, 'k', label='Projection')
     ax_loop.plot(bias_data, guess_data, 'g', label='Guess')
@@ -616,8 +630,8 @@ def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit,
 
     def update_loop_plots(loop_field, **kwargs):
         loop_ind = kwargs['Loop Number']
-        spatial_map = fit_3d[:,:, loop_ind][loop_field]
-        im_map.set_data(spatial_map)
+        spatial_map = fit_3d[:, :, loop_ind][loop_field]
+        im_map.set_data(spatial_map.T)
         spat_mean = np.mean(spatial_map)
         spat_std = np.std(spatial_map)
         im_map.set_clim(vmin=spat_mean - 3 * spat_std, vmax=spat_mean + 3 * spat_std)
@@ -627,10 +641,10 @@ def jupyter_visualize_beps_loops(h5_projected_loops, h5_loop_guess, h5_loop_fit,
         main_vert_line.set_xdata((kwargs['X'], kwargs['X']))
         main_hor_line.set_ydata((kwargs['Y'], kwargs['Y']))
 
-        proj_data = proj_nd_shifted[row_ind, col_ind, :, loop_ind]
+        proj_data = proj_nd_shifted[col_ind, row_ind, :, loop_ind]
         bias_data = bias_shifted[:, loop_ind]
-        guess_data = loop_fit_function(bias_data, np.array(list(guess_3d[row_ind, col_ind, loop_ind])))
-        fit_data = loop_fit_function(bias_data, np.array(list(fit_3d[row_ind, col_ind, loop_ind])))
+        guess_data = loop_fit_function(bias_data, np.array(list(guess_3d[col_ind, row_ind, loop_ind])))
+        fit_data = loop_fit_function(bias_data, np.array(list(fit_3d[col_ind, row_ind, loop_ind])))
         for line_handle, data in zip(line_handles, [proj_data, guess_data, fit_data]):
             line_handle.set_ydata(data)
         ax_loop.set_ylim([np.min([proj_data, guess_data, fit_data]), np.max([proj_data, guess_data, fit_data])])
