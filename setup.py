a3c0364b15ea1cb81a8b79ab7fc2b6fede8e8ad5
Unknown <csmith55@utk.edu>
2017-10-09 09:02:54 -0400

    Increment version for PycroDataset release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
     name='pycroscopy',
-    version='0.57.00',
+    version='0.58.00',
     description='A suite of Python libraries for high performance scientific computing of microscopy data.',
     long_description=long_description,
     classifiers=[
