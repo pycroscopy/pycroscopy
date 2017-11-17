bf7af276d4db15b611abb0cc97e3cd8471e23f00
Unknown <csmith55@utk.edu>
2017-11-17 10:49:41 -0500

    Reformat requirements

--- setup.py
+++ setup.py
@@ -10,13 +10,23 @@ with open(os.path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 if on_rtd:
-    requirements = ['psutil', 'xlrd>=1.0.0']
+    requirements = ['psutil',
+                    'xlrd>=1.0.0']
 else:
-    requirements = ['numpy_groupies>=0.9.6', 'pyqtgraph>=0.10',
-                    'h5py>=2.6.0', 'igor', 'matplotlib>=2.0.0',
-                    'scikit-learn>=0.17.1', 'xlrd>=1.0.0','joblib>=0.11',
-                    'psutil', 'scikit-image>=0.12.3', 'scipy>=0.17.1',
-                    'numpy>=1.11.0', 'ipywidgets>=5.2.2', 'ipython>=5.1.0']
+    requirements = ['numpy_groupies>=0.9.6',
+                    'pyqtgraph>=0.10',
+                    'h5py>=2.6.0',
+                    'igor',
+                    'matplotlib>=2.0.0',
+                    'scikit-learn>=0.17.1',
+                    'xlrd>=1.0.0',
+                    'joblib>=0.11',
+                    'psutil',
+                    'scikit-image>=0.12.3',
+                    'scipy>=0.17.1',
+                    'numpy>=1.11.0',
+                    'ipywidgets>=5.2.2',
+                    'ipython>=5.1.0']
 
 setup(
     name='pycroscopy',
