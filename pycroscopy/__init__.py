4783fb337aeebe0370b1345aab8f96a11c1b556e
syz <somnaths@ornl.gov>
2018-01-19 16:23:51 -0500

    Only importing core when importing pycroscopy

--- pycroscopy/__init__.py
+++ pycroscopy/__init__.py
@@ -13,20 +13,10 @@ Submodules
     viz
 
 """
-from . import analysis
-from .analysis import *
-from . import io
-from .io import *
-from . import processing
-from .processing import *
-from . import viz
-from .viz import *
+from . import core
+from .core import *
 
 from .__version__ import version as __version__
 from .__version__ import time as __time__
 
-__all__ = ['processing', 'analysis', 'io', 'viz', '__time__', '__version__']
-__all__ += io.__all__
-__all__ += processing.__all__
-__all__ += analysis.__all__
-__all__ += viz.__all__
+__all__ = core.__all__
