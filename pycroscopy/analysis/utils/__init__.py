39c93ba94a5e574ecc69d46bded24b3f85031a46
syz <somnaths@ornl.gov>
2018-01-20 16:34:35 -0500

    corrected import for tree

--- pycroscopy/analysis/utils/__init__.py
+++ pycroscopy/analysis/utils/__init__.py
@@ -1,7 +1,6 @@
 from . import be_loop
 from . import be_sho
-from . import tree
 from . import atom_finding
 from . import giv_utils
 
-__all__ = ['be_sho', 'be_loop', 'tree', 'atom_finding', 'giv_utils', 'atom_finding_general_gaussian']
+__all__ = ['be_sho', 'be_loop', 'atom_finding', 'giv_utils', 'atom_finding_general_gaussian']
