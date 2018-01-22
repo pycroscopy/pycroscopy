Fix units bug in SVD class

pycroscopy/processing/svd_utils.py
@@ -91,10 +91,10 @@ class SVD(Process):
 
         ds_S = MicroDataset('S', data=np.float32(S))
         ds_S.attrs['labels'] = {'Principal Component': [slice(0, None)]}
-        ds_S.attrs['units'] = ''
+        ds_S.attrs['units'] = ['']
         ds_inds = MicroDataset('Component_Indices', data=np.uint32(np.arange(len(S))))
         ds_inds.attrs['labels'] = {'Principal Component': [slice(0, None)]}
-        ds_inds.attrs['units'] = ''
+        ds_inds.attrs['units'] = ['']
         del S
 
         u_chunks = calc_chunks(U.shape, np.float32(0).itemsize)