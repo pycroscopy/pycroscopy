2b260fb3362b68c66b7dffeb2d15501dfe704f3a
syz <somnaths@ornl.gov>
2018-02-08 10:15:22 -0500

    Test added for reshape_from_n_dims with missing inputs

--- tests/core/io/test_hdf_utils.py
+++ tests/core/io/test_hdf_utils.py
@@ -1005,7 +1005,43 @@ class TestHDFUtils(unittest.TestCase):
         self.assertTrue(success)
         self.assertTrue(np.allclose(ret_2d, expected_2d))
 
+    def test_reshape_from_n_dims_pos_and_spec_may_may_not_be_provided(self):
+        num_rows = 3
+        num_cols = 5
+        num_cycles = 2
+        num_cycle_pts = 7
 
+        # the N dimensional dataset should be arranged in the following order:
+        # [positions slowest to fastest, spectroscopic slowest to fastest]
+        source_nd = np.zeros(shape=(num_rows, num_cols, num_cycles, num_cycle_pts), dtype=np.float16)
+        expected_2d = np.zeros(shape=(num_rows * num_cols, num_cycle_pts * num_cycles), dtype=np.float16)
+        for row_ind in range(num_rows):
+            for col_ind in range(num_cols):
+                for cycle_ind in range(num_cycles):
+                    for bias_ind in range(num_cycle_pts):
+                        val = 1E+3 * row_ind + 1E+2 * col_ind + 1E+1 * cycle_ind + bias_ind
+                        expected_2d[row_ind * num_cols + col_ind, cycle_ind * num_cycle_pts + bias_ind] = val
+                        source_nd[row_ind, col_ind, cycle_ind, bias_ind] = val
+
+        source_pos_data = np.vstack((np.tile(np.arange(num_cols), num_rows),
+                                     np.repeat(np.arange(num_rows), num_cols))).T
+        source_spec_data = np.vstack((np.tile(np.arange(num_cycle_pts), num_cycles),
+                                      np.repeat(np.arange(num_cycles), num_cycle_pts)))
+
+        # case 1: only pos provided:
+        ret_2d, success = hdf_utils.reshape_from_n_dims(source_nd, h5_pos=source_pos_data, verbose=True)
+        self.assertTrue(success)
+        self.assertTrue(np.allclose(ret_2d, expected_2d))
+
+        # case 2: only spec provided:
+        ret_2d, success = hdf_utils.reshape_from_n_dims(source_nd, h5_spec=source_spec_data, verbose=True)
+        self.assertTrue(success)
+        self.assertTrue(np.allclose(ret_2d, expected_2d))
+
+        # case 3: neither pos nor spec provided:
+        ret_2d, success = hdf_utils.reshape_from_n_dims(source_nd, verbose=True)
+        self.assertTrue(success)
+        self.assertTrue(np.allclose(ret_2d, expected_2d))
 
     """  
     def test_calc_chunks(self):
