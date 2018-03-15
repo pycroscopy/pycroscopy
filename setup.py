6dc8b5d35c77df0f6c6ca990ffa1c7a9c5e953a7
Unknown <csmith55@utk.edu>
2018-03-15 10:04:56 -0400

    Version update for tag

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = ['numpy_groupies>=0.9.6',
 
 setup(
     name='pycroscopy',
-    version='0.59.4',
+    version='0.59.5',
     description='Python library for scientific analysis of microscopy data',
     long_description=long_description,
     classifiers=[
