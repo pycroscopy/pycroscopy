e46397c3d9e94fb5f6d4adcf65b954ed228d3348
syz <somnaths@ornl.gov>
2018-03-25 18:13:05 -0400

    Fixed a few obvious tests that fail because of the number of cores

--- tests/core/io/test_io_utils.py
+++ tests/core/io/test_io_utils.py
@@ -19,7 +19,7 @@ class TestIOUtils(unittest.TestCase):
     def test_recommend_cores_many_small_jobs(self):
         num_jobs = 14035
         ret_val = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False)
-        self.assertEqual(ret_val, MAX_CPU_CORES-2)
+        self.assertEqual(ret_val, max(1, MAX_CPU_CORES-2))
         ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=1, lengthy_computation=False)
         self.assertEqual(ret_val, 1)
         ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES, lengthy_computation=False)
@@ -37,9 +37,9 @@ class TestIOUtils(unittest.TestCase):
     def test_recommed_cores_few_large_jobs(self):
         num_jobs = 13
         ret_val = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=True)
-        self.assertEqual(ret_val, MAX_CPU_CORES-2)
+        self.assertEqual(ret_val, max(1, MAX_CPU_CORES-2))
         ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES - 1, lengthy_computation=True)
-        self.assertEqual(ret_val, MAX_CPU_CORES - 1)
+        self.assertEqual(ret_val, max(1, MAX_CPU_CORES - 1))
 
     def test_formatted_str_to_number(self):
         self.assertEqual(io_utils.formatted_str_to_number("4.32 MHz", ["MHz", "kHz"], [1E+6, 1E+3]), 4.32E+6)
