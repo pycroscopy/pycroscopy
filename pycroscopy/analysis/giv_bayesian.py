4df5a51c1df1742590fb0489a271aedf8ef62760
syz <somnaths@ornl.gov>
2018-03-26 20:33:26 -0400

    Uses new build_ind_vals()

--- pycroscopy/analysis/giv_bayesian.py
+++ pycroscopy/analysis/giv_bayesian.py
@@ -12,8 +12,8 @@ import numpy as np
 from ..core.processing.process import Process, parallel_compute
 from ..core.io.virtual_data import VirtualDataset, VirtualGroup
 from ..core.io.dtype_utils import stack_real_to_compound
-from ..core.io.hdf_utils import get_h5_obj_refs, get_auxillary_datasets, copy_attributes, link_as_main, \
-    build_ind_val_dsets
+from ..core.io.hdf_utils import get_h5_obj_refs, get_auxillary_datasets, copy_attributes, link_as_main
+from ..core.io.write_utils import build_ind_val_dsets, AuxillaryDescriptor
 from ..core.io.hdf_writer import HDFwriter
 from .utils.giv_utils import do_bayesian_inference, bayesian_inference_on_period
 
@@ -135,16 +135,15 @@ class GIVBayesian(Process):
 
         if self.verbose:
             print('Now creating the datasets')
-
-        ds_spec_inds, ds_spec_vals = build_ind_val_dsets([self.num_x_steps], is_spectral=True,
-                                                         labels=['Bias'], units=['V'], verbose=self.verbose)
+        spec_desc = AuxillaryDescriptor([self.num_x_steps], ['Bias'], ['V'])
+        ds_spec_inds, ds_spec_vals = build_ind_val_dsets(spec_desc, is_spectral=True, verbose=self.verbose)
 
         cap_shape = (num_pos, 1)
         ds_cap = VirtualDataset('Capacitance', data=[], maxshape=cap_shape, dtype=cap_dtype, chunking=cap_shape,
                                 compression='gzip')
         ds_cap.attrs = {'quantity': 'Capacitance', 'units': 'pF'}
-        ds_cap_spec_inds, ds_cap_spec_vals = build_ind_val_dsets([1], is_spectral=True,
-                                                                 labels=['Direction'], units=[''], verbose=self.verbose)
+        cap_spec_desc = AuxillaryDescriptor([1], ['Direction'], [''])
+        ds_cap_spec_inds, ds_cap_spec_vals = build_ind_val_dsets(cap_spec_desc, is_spectral=True, verbose=self.verbose)
         # the names of these datasets will clash with the ones created above. Change names manually:
         ds_cap_spec_inds.name = 'Spectroscopic_Indices_Cap'
         ds_cap_spec_vals.name = 'Spectroscopic_Values_Cap'
