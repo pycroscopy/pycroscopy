4100192c17c00e982576cd2e5542485c469df67b
Carlo <carlo.dri@gmail.com>
2018-03-28 09:07:26 +0200

    adopt environment markers (PEP508) in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from codecs import open
 import os
-import sys
 
 from setuptools import setup, find_packages
 
@@ -24,16 +23,10 @@ requirements = ['numpy_groupies>=0.9.6',
                 'scipy>=0.17.1',
                 'numpy>=1.11.0',
                 'ipywidgets>=5.2.2',
+                'ipython>=5,<6;python_version<"3.3"',  # IPython 6.0+ does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2
+                'ipython>=6.0;python_version>="3.3"', # Beginning with IPython 6.0, Python 3.3 and above is required.
                ]
-"""IPython 6.0+ does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2.
-When using Python 2.7, please install IPython 5.x LTS Long Term Support version.
-Beginning with IPython 6.0, Python 3.3 and above is required."""
-if sys.version_info.major == 2:
-    requirements.append('ipython>=5,<6')
-    requirements.append('unittest2')
-elif sys.version_info.major == 3:
-    requirements.append('ipython>=6')
-    
+
 setup(
     name='pycroscopy',
     version=__version__,
