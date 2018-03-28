257ad74dcbe372576bdd4e753d99452fa03e6480
Carlo <carlo.dri@gmail.com>
2018-03-28 09:42:14 +0200

    better ipython dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = ['numpy_groupies>=0.9.6',
                 'scipy>=0.17.1',
                 'numpy>=1.11.0',
                 'ipywidgets>=5.2.2',
-                'ipython>=5,<6;python_version<"3.3"',  # IPython 6.0+ does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2
+                'ipython>=5.1.0,<6;python_version<"3.3"',  # IPython 6.0+ does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2
                 'ipython>=6.0;python_version>="3.3"', # Beginning with IPython 6.0, Python 3.3 and above is required.
                 'unittest2;python_version<"3.0"',
                ]
