7b7a4f70694e39011c2dcd3aca3fd648aea25090
syz <somnaths@ornl.gov>
2018-04-25 19:53:45 -0400

    Slightly revised version of existing cookbook. FOUR functions need to be added

--- examples/cookbooks/plot_plot_utils.py
+++ examples/cookbooks/plot_plot_utils.py
@@ -4,7 +4,8 @@ Plotting utilities
 ======================================================================================
 
 **Suhas Somnath**
-10/12/2017
+
+8/12/2017
 
 This is a short walkthrough of useful plotting utilities available in pycroscopy. 
 Some of these functions fill gaps in the defult matplotlib package, some were 
@@ -31,8 +32,8 @@ except ImportError:
 ################################################################################################
 # 1D plot utilities
 # ===========================
-# plot_loops
-# ----------
+# plot_curves()
+# ------------
 # This function is particularly useful when we need to plot a 1D signal acquired at multiple locations.
 # The function is rather flexible and can take on several optional arguments that will be alluded to below
 # In the below example, we are simply simulating sine waveforms for different frequencies (think of these as
@@ -49,7 +50,7 @@ px.plot_utils.plot_curves(x_vec, y_mat)
 
 ################################################################################################
 # Frequently, we may need to compare signals from two different datasets for the same positions
-# The same plot_loops function can be used for this purpose even if the signal lengths / resolutions are different
+# The same plot_curves function can be used for this purpose even if the signal lengths / resolutions are different
 
 x_vec_1 = np.linspace(0, 2*np.pi, 256)
 x_vec_2 = np.linspace(0, 2*np.pi, 32)
@@ -61,7 +62,7 @@ px.plot_utils.plot_curves([x_vec_1, x_vec_2], [y_mat_1, y_mat_2],
                           title='Sine and Cosine of different resolutions')
 
 ################################################################################################
-# plot_line_family
+# plot_line_family()
 # ------------------
 # Often there is a need to visualize multiple spectra or signals on the same plot. plot_line_family
 # is a handy function ideally suited for this purpose and it is highly configurable for different styles and purposes
@@ -91,8 +92,8 @@ px.plot_utils.plot_line_family(axes[2], x_vec, y_mat,
 axes[2].set_title('Line family with colorbar')
 
 ################################################################################################
-# plot_complex_loop_stack
-# ------------------
+# plot_complex_spectra()
+# ----------------------
 # This handy function plots the amplitude and phase components of multiple complex valued spectra
 # Here we simulate the signal coming from a simple harmonic oscillator (SHO). 
 
@@ -110,8 +111,8 @@ for index, amp, freq, qfac, phase in zip(range(num_spectra), amps, freqs, q_facs
 fig, axis = px.plot_utils.plot_complex_spectra(spectra, w_vec, title='Oscillator responses')
 
 ################################################################################################
-# rainbow_plot
-# ------------
+# rainbow_plot()
+# --------------
 # This function is ideally suited for visualizing a signal that varies as a function of time or when 
 # the directionality of the signal is important
 
@@ -124,8 +125,8 @@ px.plot_utils.rainbow_plot(axis, np.cos(t_vec)*np.linspace(0, 1, num_pts),
                            num_steps=32)
 
 ################################################################################################
-# cbar_for_line_plot
-# ------------------
+# cbar_for_line_plot()
+# ---------------------
 # Note that from the above plot it may not be clear if the signal is radiating outwards or spiraling inwards.
 # In these cases it helps to add a colorbar. However, colorbars can typically only be added for 2D images.
 # In such cases we can use a handy function: cbar_for_line_plot
@@ -142,8 +143,8 @@ cbar = px.plot_utils.cbar_for_line_plot(axis, 10)
 cbar.set_label('Time (sec)')
 
 ################################################################################################
-# plot_scree
-# ------------------
+# plot_scree()
+# ------------
 # One of the results of applying Singular Value Decomposition is the variance or statistical significance
 # of the resultant components. This data is best visualized via a log-log plot and plot_scree is available
 # exclusively to vvisualizethis kind of data
@@ -152,24 +153,22 @@ scree = np.exp(-1 * np.arange(100))
 px.plot_utils.plot_scree(scree, color='r')
 
 ################################################################################################
-# 2D plot utilities
-# ===========================
 # Colormaps
-# ---------
-# pycroscopy has a handful of colormaps suited for different applications.
+# =========
+# pycroscopy.plot_utils has a handful of colormaps suited for different applications.
 #
-# cmap_jet_white_center
-# ~~~~~~~~~~~~~~~~~~~~~
+# cmap_jet_white_center()
+# -----------------------
 # This is the standard jet colormap with a white center instead of green. This is a good colormap for images with 
 # divergent data (data that diverges slightly both positively and negatively from the mean). 
 # One example target is the ronchigrams from scanning transmission electron microscopy (STEM)
 # 
-# cmap_hot_desaturated
-# ~~~~~~~~~~~~~~~~~~~~~
+# cmap_hot_desaturated()
+# ----------------------
 # This is a desaturated version of the  standard jet colormap
 #
-# discrete_cmap
-# ~~~~~~~~~~~~~~~~~~~~~
+# discrete_cmap()
+# ---------------
 # This function helps create a discretized version of the provided colormap. This is ideally suited when the data
 # only contains a few discrete values. One popular application is the visualization of labels from a clustering
 # algorithm
@@ -196,8 +195,8 @@ for axis, title, cmap in zip(axes.flat,
 fig.tight_layout()
 
 ################################################################################################
-# make_linear_alpha_cmap
-# --------
+# make_linear_alpha_cmap()
+# ------------------------
 # On certain occasions we may want to superimpose one image with another. However, this is not possible
 # by default since colormaps involve solid colors. This function allows one to plot multiple images using
 # a transparent-to-solid colormap. Here we will demonstrate this by plotting blobs representing atomic columns
@@ -231,11 +230,23 @@ cbar = plt.colorbar(im_handle, ax=axis, orientation='vertical',
                         fraction=0.046, pad=0.04, use_gridspec=True)
 
 ################################################################################################
-# plot_map
-# --------
+# get_cmap_object()
+# -----------------
+# This function is useful more for developers writing their own plotting functions that need to manipulate the
+# colormap object. This function makes it easy to ensure that you are working on the colormap object and not the
+# string name of the colormap (both of which are accepted by most matplotlib functions).
+# Here we simply compare the returned values when passing both the colormap object and the string name of the colormap
+
+px.plot_utils.get_cmap_object('jet') == px.plot_utils.get_cmap_object(plt.cm.jet)
+
+################################################################################################
+# 2D plot utilities
+# ===========================
+# plot_map()
+# -----------
 # This function adds several popularly used features to the basic image plotting function in matplotlib including:
 # * easy addition of a colorbar
-# * easy modification of the x and y tick values
+# * custom x and y tick values
 # * clipping the colorbar to N standard deviations of the mean
 
 x_vec = np.linspace(0, 6*np.pi, 256)
@@ -254,10 +265,53 @@ axes[1].set_title('plot_map')
 fig.tight_layout()
 
 ################################################################################################
-# set_tick_font_size
-# ------------------
+# plot_map_stack()
+# -----------------
+# One of the most popular operations in scientific research is the visualization of a stack of images.
+# This function is built specifically for that purpose. 
+# Here we simply simulate some images using sinusoidal functions for demonstration purposes.
+
+
+def get_sine_2d_image(freq):
+    x_vec = np.linspace(0, freq*np.pi, 256)
+    y_vec = np.sin(x_vec)**2
+    return y_vec * np.atleast_2d(y_vec).T
+
+
+frequencies = [0.25, 0.5, 1, 2, 4 ,8, 16, 32, 64]
+image_stack = [get_sine_2d_image(freq) for freq in frequencies]
+image_stack = np.array(image_stack)
+
+fig, axes = px.plot_utils.plot_map_stack(image_stack, reverse_dims=False, fig_title_yoffset=0.95)
+
+################################################################################################
+# plot_complex_spectra()
+# ----------------------
+# This function plots the amplitude and phase components of a stack of complex valued 2D images.
+# Here we simulate the data using sine and cosine components
+
+
+def get_complex_2d_image(freq):
+    # Simple function to generate images
+    x_vec = np.linspace(0, freq*np.pi, 256)
+    y_vec_1 = np.sin(x_vec)**2
+    y_vec_2 = np.cos(x_vec)**2
+    return y_vec_2 * np.atleast_2d(y_vec_2).T + 1j*(y_vec_1 * np.atleast_2d(y_vec_1).T)
+
+
+# The range of frequences over which the images are generated
+frequencies = 2 ** np.arange(4)
+image_stack = [get_complex_2d_image(freq) for freq in frequencies]
+
+fig, axes = px.plot_utils.plot_complex_spectra(np.array(image_stack))
+
+################################################################################################
+# General Utilities
+# =================
+# set_tick_font_size()
+# --------------------
 # Adjusting the font sizes of the tick marks is often necessary for preparing figures for journal papers.
-# However, adjusting the tick sizes is actually tedious in python and this function makes this easier. 
+# However, adjusting the tick sizes is actually tedious in python and this function makes this easier.
 
 test = np.random.rand(10, 10)
 
@@ -269,49 +323,8 @@ for axis, title in zip(axes, ['Default', 'Custom']):
 px.plot_utils.set_tick_font_size(axes[1], 24)
 fig.tight_layout()
 
-################################################################################################
-# plot_map_stack
-# ---------------
-# One of the most popular operations in scientific research is the visualization of a stack of images.
-# This function is built specifically for that purpose. 
-# Here we simply simulate some images using sinusoidal functions for demonstration purposes.
-
-def get_sine_2d_image(freq):
-    x_vec = np.linspace(0, freq*np.pi, 256)
-    y_vec = np.sin(x_vec)**2
-    return y_vec * np.atleast_2d(y_vec).T
-
-frequencies = [0.25, 0.5, 1, 2, 4 ,8, 16, 32, 64]
-image_stack = [get_sine_2d_image(freq) for freq in frequencies]
-image_stack = np.array(image_stack)
-
-fig, axes = px.plot_utils.plot_map_stack(image_stack, reverse_dims=False, fig_title_yoffset=0.95)
-
-################################################################################################
-# plot_complex_map_stack
-# ---------------
-# This function plots the amplitude and phase components of a stack of complex valued 2D images.
-# Here we simulate the data using sine and cosine components
-
-# Simple function to generate images
-def get_complex_2d_image(freq):
-    x_vec = np.linspace(0, freq*np.pi, 256)
-    y_vec_1 = np.sin(x_vec)**2
-    y_vec_2 = np.cos(x_vec)**2
-    return y_vec_2 * np.atleast_2d(y_vec_2).T + 1j*(y_vec_1 * np.atleast_2d(y_vec_1).T)
-
-# The range of frequences over which the images are generated
-frequencies = 2**np.arange(4)
-image_stack = [get_complex_2d_image(freq) for freq in frequencies]
-
-fig, axes = px.plot_utils.plot_complex_map_stack(np.array(image_stack))
-
-################################################################################################
-# get_cmap_object
-# ---------------
-# This function is useful more for developers writing their own plotting functions that need to manipulate the
-# colormap object. This function makes it easy to ensure that you are working on the colormap object and not the
-# string name of the colormap (both of which are accepted by most matplotlib functions).
-# Here we simply compare the returned values when passing both the colormap object and the string name of the colormap
-
-px.plot_utils.get_cmap_object('jet') == px.plot_utils.get_cmap_object(plt.cm.jet)
+# export_fig_data()
+# cmap_from_rgba()
+# get_plot_grid_size
+# make_scalar_mappable
+# use_nice_plot_params
