1f935ec8947e8417daf888f5b989df6b31731085
Chris Smith <csmith55@utk.edu>
2018-04-25 10:09:31 -0400

    Updated copy_region_ref to work on all datasets
    Previously only worked on 'main' datasets

--- pycroscopy/core/io/hdf_utils.py
+++ pycroscopy/core/io/hdf_utils.py
@@ -1478,42 +1478,52 @@ def copy_region_refs(h5_source, h5_target):
     '''
     Check both h5_source and h5_target to ensure that are Main
     '''
-    if not all([check_if_main(h5_source), check_if_main(h5_target)]):
-        raise TypeError('Inputs to copyRegionRefs must be HDF5 Datasets.')
+    are_main = all([check_if_main(h5_source), check_if_main(h5_target)])
+    if not all([isinstance(h5_source, h5py.Dataset), isinstance(h5_target, h5py.Dataset)]):
+        raise TypeError('Inputs to copyRegionRefs must be HDF5 Datasets or PycroDatasets.')
 
-    h5_source_inds = h5_source.file[h5_source.attrs['Spectroscopic_Indices']]
+    if are_main:
+        h5_source_inds = h5_source.file[h5_source.attrs['Spectroscopic_Indices']]
 
-    h5_spec_inds = h5_target.file[h5_target.attrs['Spectroscopic_Indices']]
-    h5_spec_vals = h5_target.file[h5_target.attrs['Spectroscopic_Values']]
+        h5_spec_inds = h5_target.file[h5_target.attrs['Spectroscopic_Indices']]
+        h5_spec_vals = h5_target.file[h5_target.attrs['Spectroscopic_Values']]
 
     for key in h5_source.attrs.keys():
-        # TODO: If this is BE specific, it does NOT belong here
-        if '_Plot_Group' not in key:
+        if not isinstance(h5_source.attrs[key], h5py.RegionReference):
             continue
 
-        if h5_source_inds.shape[0] == h5_spec_inds.shape[0]:
+        if are_main:
+            if h5_source_inds.shape[0] == h5_spec_inds.shape[0]:
+                '''
+                Spectroscopic dimensions are identical.
+                Do direct copy.
+                '''
+                ref_inds = simple_region_ref_copy(h5_source, h5_target, key)
+
+            else:
+                '''
+                Spectroscopic dimensions are different.
+                Do the dimension reducing copy.
+                '''
+                ref_inds = copy_reg_ref_reduced_dim(h5_source, h5_target, h5_source_inds, h5_spec_inds, key)
+
             '''
-            Spectroscopic dimensions are identical.
-            Do direct copy.
+            Create references for Spectroscopic Indices and Values
+            Set the end-point of each hyperslab in the position dimension to the number of
+            rows in the index array
             '''
-            ref_inds = simple_region_ref_copy(h5_source, h5_target, key)
+
+            ref_inds[:, 1, 0][ref_inds[:, 1, 0] > h5_spec_inds.shape[0]] = h5_spec_inds.shape[0] - 1
+            spec_inds_ref = create_region_reference(h5_spec_inds, ref_inds)
+            h5_spec_inds.attrs[key] = spec_inds_ref
+            spec_vals_ref = create_region_reference(h5_spec_vals, ref_inds)
+            h5_spec_vals.attrs[key] = spec_vals_ref
 
         else:
             '''
-            Spectroscopic dimensions are different.
-            Do the dimension reducing copy.
+            If not main datasets, then only simple copy can be used.
             '''
-            ref_inds = copy_reg_ref_reduced_dim(h5_source, h5_target, h5_source_inds, h5_spec_inds, key)
-        '''
-        Create references for Spectroscopic Indices and Values
-        Set the end-point of each hyperslab in the position dimension to the number of
-        rows in the index array
-        '''
-        ref_inds[:, 1, 0][ref_inds[:, 1, 0] > h5_spec_inds.shape[0]] = h5_spec_inds.shape[0] - 1
-        spec_inds_ref = create_region_reference(h5_spec_inds, ref_inds)
-        h5_spec_inds.attrs[key] = spec_inds_ref
-        spec_vals_ref = create_region_reference(h5_spec_vals, ref_inds)
-        h5_spec_vals.attrs[key] = spec_vals_ref
+            simple_region_ref_copy(h5_source, h5_target, key)
 
 
 def copy_reg_ref_reduced_dim(h5_source, h5_target, h5_source_inds, h5_target_inds, key):
