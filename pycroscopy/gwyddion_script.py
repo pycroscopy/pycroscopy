7d1ecad41cb180763d637fbe617bede678dada35
str-eat <danielchemie@outlook.com>
2018-06-16 22:10:47 -0500

    file edits

--- pycroscopy/gwyddion_script.py
+++ pycroscopy/gwyddion_script.py
@@ -1,8 +1,9 @@
 import pycroscopy as px
 import gwyfile as gw
 
+
 if __name__ == "__main__":
-    input_file_gsf = '../../../data/chip.gsf'
+    input_file_gsf = '../data/chip.gsf'
     print(px.__version__)
     # input_file_gwy = '../../../data/samples.gwy'
     # metadata, data = gsf_read.gsf_read(input_file_gsf)
