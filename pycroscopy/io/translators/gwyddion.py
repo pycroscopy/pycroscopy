449d3811aa3461ce12901d02ec5239e4b511c1d8
str-eat <danielchemie@outlook.com>
2018-06-26 22:34:05 -0700

    June 26th Commit - gwyddion image translate close to finalized

--- pycroscopy/io/translators/gwyddion.py
+++ pycroscopy/io/translators/gwyddion.py
@@ -92,8 +92,8 @@ class GwyddionTranslator(Translator):
         # data_type, comments and experiment_date to file-level parms
         # Using pop, move some global parameters from gsf_meta to global_parms:
         self.global_parms['data_type'] = 'Gwyddion_GSF'
-        self.global_parms['comments'] = gsf_meta.pop('comment', '')
-        self.global_parms['experiment_date'] = gsf_meta.pop('date', '')
+        self.global_parms['comments'] = gsf_meta.get('comment', '')
+        self.global_parms['experiment_date'] = gsf_meta.get('date', '')
 
         # overwrite some parameters at the file level:
         write_simple_attrs(meas_grp.parent, self.global_parms)
@@ -101,18 +101,18 @@ class GwyddionTranslator(Translator):
         # Build the reference values for the ancillary position datasets:
         # TODO: Remove information from parameters once it is used meaningfully where it needs to be.
         # Here, it is no longer necessary to save XReal anymore so we will pop (remove) it from gsf_meta
-        x_offset = gsf_meta.pop('XOffset', 0)
-        x_range = gsf_meta.pop('XReal', 1.0)
+        x_offset = gsf_meta.get('XOffset', 0)
+        x_range = gsf_meta.get('XReal', 1.0)
         # TODO: Use Numpy wherever possible instead of pure python
-        x_vals = np.linspace(0, x_range, gsf_meta.pop('XRes')) + x_offset
+        x_vals = np.linspace(0, x_range, gsf_meta.get('XRes')) + x_offset
 
-        y_offset = gsf_meta.pop('YOffset', 0)
-        y_range = gsf_meta.pop('YReal', 1.0)
-        y_vals = np.linspace(0, y_range, gsf_meta.pop('YRes')) + y_offset
+        y_offset = gsf_meta.get('YOffset', 0)
+        y_range = gsf_meta.get('YReal', 1.0)
+        y_vals = np.linspace(0, y_range, gsf_meta.get('YRes')) + y_offset
 
         # Just define the ancillary position and spectral dimensions. Do not create datasets yet
         pos_desc = [Dimension('X', gsf_meta.get('XYUnits', 'arb. units'), x_vals),
-                    Dimension('Y', gsf_meta.pop('XYUnits', 'arb. units'), y_vals)]
+                    Dimension('Y', gsf_meta.get('XYUnits', 'arb. units'), y_vals)]
 
         spec_desc = Dimension('Intensity', gsf_meta.get('ZUnits', 'arb. units'), [1])
 
@@ -131,7 +131,7 @@ class GwyddionTranslator(Translator):
         write_main_dataset(chan_grp,
                            np.atleast_2d(np.reshape(two_dim_image,
                            len(pos_desc[0].values) * len(pos_desc[1].values))).transpose(),
-                           'Raw_Data', gsf_meta.pop('Title', 'Unknown'), gsf_meta.pop('ZUnits', 'arb. units'),
+                           'Raw_Data', gsf_meta.get('Title', 'Unknown'), gsf_meta.get('ZUnits', 'arb. units'),
                            pos_desc, spec_desc)
         # TODO: When passing optional arguments, you are HIGHLY recommended to specify the variable name such as aux_pos_prefix='Position_' instead of just 'Position_' (which is how you pass regulard arguments)
 
@@ -164,10 +164,9 @@ class GwyddionTranslator(Translator):
                     self._translate_graph(meas_grp, gwy_data)
                 elif obj.endswith('data'):
                     self.global_parms['data_type'] = 'GwyddionGWY_' + 'Image'
-                    print('Entering translation')
-                    self._translate_image_stack(meas_grp, gwy_data)
-                    print('Translate executed')
-                break
+                    self._translate_image_stack(meas_grp, gwy_data, obj)
+                else:
+                    continue
             except ValueError:
                 # if the second index of the gwy_key cannot be cast into an int
                 # then it needs to be processed wither as a spectra, volume or xyz
@@ -181,8 +180,6 @@ class GwyddionTranslator(Translator):
                 elif gwy_key[1] == 'xyz':
                     self.global_parms['data_type'] = 'GwyddionGWY_' + 'XYZ'
                     self._translate_xyz(meas_grp, gwy_data)
-
-        print("Out of loop")
         write_simple_attrs(meas_grp.parent, self.global_parms)
 
         # TODO: Use the Bruker translator as a reference. use the three functions below as necessary to keep the code clean and easy to read.
@@ -197,61 +194,67 @@ class GwyddionTranslator(Translator):
         # Prepare the list of raw_data datasets
 
 
-    def _translate_image_stack(self, meas_grp, gwy_data):
+    def _translate_image_stack(self, meas_grp, gwy_data, obj):
         """
         Use this function to write data corresponding to a stack of scan images (most common)
         Returns
         -------
         """
         # Need to build a set of channels to test against and a function-level variable to write to
-        channels = []
+        channels = {}
         current_channel = ''
-
+        
         # Iterate through each object in the gwy dataset
-        for obj in gwy_data:
-            gwy_key = obj.split('/')
-            # Test whether a new channel needs to be created
-            # The 'filename' structure in the gwy file should not have a channel created hence the try/except block
-            try:
-                if int(gwy_key[1]) not in channels:
-                    channels.append(int(gwy_key[1]))
-                    current_channel = create_indexed_group(meas_grp, "Channel")
-            except ValueError:
-                if obj.endswith('filename'):
-                    # print("Reached filename", obj, gwy_data[obj])
-                    # image filename processing
-                    pass
+        gwy_key = obj.split('/')
+        # Test whether a new channel needs to be created
+        # The 'filename' structure in the gwy file should not have a channel created hence the try/except block
+        try:
+            if int(gwy_key[1]) not in channels.keys():
+                print(obj)                
+                current_channel = create_indexed_group(meas_grp, "Channel")
+                channels[int(gwy_key[1])] = current_channel
+            else:
+                print(obj)
+                current_channel = channels[int(gwy_key[1])]
+        except ValueError:
+            if obj.endswith('filename'):          
+                pass
 
-            # The data structure of the gwy file will be used to create the main dataset in the h5 file
-            if obj.endswith('data'):
-                # print('Reached data', obj, gwy_data[obj])
-                x_range = gwy_data[obj].pop('xreal', 1.0)
-                x_vals = np.linspace(0, x_range, gwy_data[obj]['xres'])
-                
-                y_range = gwy_data[obj].pop('yreal', 1.0)
-                y_vals = np.linspace(0, y_range, gwy_data[obj]['yres'])
-                
-                pos_desc = [Dimension('X', gwy_data[obj]['si_unit_xy'].get('unitstr'), x_vals),
-                    Dimension('Y', gwy_data[obj]['si_unit_xy'].pop('unitstr'), y_vals)]
+        # The data structure of the gwy file will be used to create the main dataset in the h5 file
+        if obj.endswith('data'):
+            x_range = gwy_data[obj].get('xreal', 1.0)
+            x_vals = np.linspace(0, x_range, gwy_data[obj]['xres'])
+            # print('obj {}\nx_vals {}'.format(obj, x_vals))
 
-                spec_dim = gwy_data['/{}/data/title'.format(gwy_key[1])]
-                spec_desc = Dimension(spec_dim, gwy_data[obj]['si_unit_z'].get('unitstr', 'arb. units'), [0])
-                
-                two_dim_image = gwy_data[obj]['data']
-                write_main_dataset(current_channel,
-                                    np.atleast_2d(np.reshape(two_dim_image,
-                                    len(pos_desc[0].values) * len(pos_desc[1].values))).transpose(),
-                                    'Raw_Data',
-                                    spec_dim,
-                                    gwy_data[obj]['si_unit_z'].pop('unitstr'),
-                                    pos_desc, spec_desc)
-                
-                # image data processing
-            elif obj.endswith('meta'):
-                # print('Reached meta', obj)
-                # image metadata processing for the current channel
-                meta = {}            
-                write_simple_attrs(current_channel, meta, verbose=False)
+            y_range = gwy_data[obj].get('yreal', 1.0)
+            y_vals = np.linspace(0, y_range, gwy_data[obj]['yres'])
+            
+            pos_desc = [Dimension('X',
+                        gwy_data[obj]['si_unit_xy'].get('unitstr'),
+                        x_vals),
+                        Dimension('Y', 
+                        gwy_data[obj]['si_unit_xy'].get('unitstr'),
+                        y_vals)]
+            # print(pos_desc)
+
+            spec_dim = gwy_data['/{}/data/title'.format(gwy_key[1])]
+            spec_desc = Dimension(spec_dim,
+                        gwy_data[obj]['si_unit_z'].get('unitstr', 'arb. units'),
+                        [0])
+
+            two_dim_image = gwy_data[obj]['data']
+            write_main_dataset(current_channel,
+                                np.atleast_2d(np.reshape(two_dim_image,
+                                len(pos_desc[0].values) * len(pos_desc[1].values))).transpose(),
+                                'Raw_Data',
+                                spec_dim,
+                                gwy_data[obj]['si_unit_z'].get('unitstr'),
+                                pos_desc, spec_desc)
+            # print('main dataset has been written')
+            # image data processing
+        elif obj.endswith('meta'):
+            meta = {}            
+            write_simple_attrs(current_channel, meta, verbose=False)
 
 
     def _translate_spectra(self, meas_grp, gwy_data):
