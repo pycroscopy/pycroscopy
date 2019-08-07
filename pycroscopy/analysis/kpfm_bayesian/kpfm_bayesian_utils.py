67d53caa99470857529a0a6be5e789f0d02d5eeb
Alvin Tan <alvintan2020@u.northwestern.edu>
2019-08-07 11:35:12 -0400

    added helper functions B_phin and mmlenn

--- pycroscopy/analysis/kpfm_bayesian/kpfm_bayesian_utils.py
+++ pycroscopy/analysis/kpfm_bayesian/kpfm_bayesian_utils.py
@@ -7,6 +7,7 @@
 # data streamed in from a fundamentally new data acquisition method
 # incepted by Liam and involving Kelvin probe microscopy.
 
+import scipy.optimize as spo
 import scipy.linalg as spla
 import numpy as 
 import math
@@ -15,6 +16,54 @@ import time
 def process_pixel():
 	# TODO
 	return
+
+
+def B_phin(phi, w, tt, n):
+	'''
+	phi
+	w
+	tt is a numpy vector (hopefully)
+	n
+	'''
+	N = tt.size
+	Bn = np.zeros((2*N, n+1))
+
+	for i in range(n+1):
+		Bn[1:2*N+1:2, i] = math.sin(w*tt + phi)**i
+
+	return Bn
+
+
+def mmlenn(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa):
+	'''
+	y is a numpy vector hopefully
+	'''
+	phi = pp[2]
+	sig = pp[0]
+	gam = pp[1]
+	N = y.size
+	P0 = np.diag(np.concatenate((np.array([1/sigi/sigi, 1/sigi/sigi]), 1/sig/sig*(np.arange(1, n+2)**aa))))
+	C0 = np.linalg.inv(P0)
+	GAI = 1/gam/gam*np.eye(N)
+
+	B = B_phin(phi, w, tt, n)
+	BB = AA*B*h # matrices? may be problematic
+	CC1 = np.concatenate((a1, a2, BB), axis=1)
+	CC = CC1[::2, :]
+
+	# matrix multiplication? may be problematic
+	Sig, resid, rank, s = np.linalg.lstsq(P0 + CC.T * GAI * CC, np.eye(M))
+	m_phi = Sig*(CC.T * GAI * y + P0*m0)
+
+	if (-math.pi <= phi) && (phi <= 0):
+		# matrix multiplication? may be problematic
+		out = np.sum(np.log(gam**2*np.ones((N, 1)))) + np.sum(np.log(np.linalg.eig(Sig)[0])) + /
+			  y.T * GAI * y - (Sig * CC.T * GAI * y).T * (CC.T * GAI * y) + 0*sig**2/200 # what is the point of this 0*...
+	else:
+		out = np.inf
+
+	return out
+
 
 def BayesianInference(R_H, wd, n0, p):
 	'''
@@ -102,7 +151,8 @@ def BayesianInference(R_H, wd, n0, p):
 
 	# Blackbox optimization over Phi(theta; y)
 	startTime = time.time()
-	pp1, fval, exitflag = fminsearch(@(pp)mmlen(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa), pp0) # TODO
+	#pp1, fval, exitflag = fminsearch(@(pp)mmlenn(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa), pp0) # TODO
+	pp1, fval = spo.fmin(lambda pp: mmlenn(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa), pp0)
 	optim_time_mle = time.time() - startTime
 
 	fvalg = fval
@@ -113,8 +163,10 @@ def BayesianInference(R_H, wd, n0, p):
 		pp0(2) = -math.pi*np.random.rand()
 
 		startTime = time.time()
-		options = optimset("MaxIter", 1e12, "TolFun", 1e-18, "TolX", 1e-18) # TODO
-		pp1, fval, exitflag = fminsearch(@(pp)mmlenn(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa), pp0, options) # TODO
+		#options = optimset("MaxIter", 1e12, "TolFun", 1e-18, "TolX", 1e-18) # TODO
+		#pp1, fval, exitflag = fminsearch(@(pp)mmlenn(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa), pp0, options) # TODO
+		pp1, fval = spo.fmin(lambda pp: mmlenn(pp, w, tt, AA, a1, a2, y, n, M, h, m0, sigi, aa), pp0,
+							 maxiter=int(1e12), ftol=1e-18, xtol=1e-18)
 		optim_time_mle = time.time() - startTime
 		print("pp1 is {}".format(pp1))
 		print("fval is {}".format(fval))
