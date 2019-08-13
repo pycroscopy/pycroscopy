44f763931a1c8af53b06d62cb83ee7d5f60f1b63
Alvin Tan <alvintan2020@u.northwestern.edu>
2019-08-13 13:56:06 -0400

    cleaned up bayesian code and added example script

--- pycroscopy/analysis/giv_adaptive_metropolis/bayesian_inference.py
+++ pycroscopy/analysis/giv_adaptive_metropolis/bayesian_inference.py
@@ -2,11 +2,11 @@
 """
 Runs a full Bayesian inference algorithm on spectroscopic data for ultrafast current imaging (see paper below)
 https://www.nature.com/articles/s41467-017-02455-7
+Used in conjunction with bayesian_utils.
 
 Created on Tue July 02, 2019
 
 @author: Alvin Tan, Emily Costa
-
 """
 
 from __future__ import division, print_function, absolute_import, unicode_literals
@@ -54,7 +54,9 @@ class AdaptiveBayesianInference(Process):
 	def __init__(self, h5_main, f=200, V0=None, Ns=int(7e7), M=101, parse_mod=1, **kwargs):
 		"""
 		Bayesian inference is done on h5py dataset object that has already been filtered
-		and reshaped.
+			and reshaped.
+
+		Parameters
 		----------
 		h5_main 	: h5py.Dataset object
 			Dataset to process. Must be a USID main dataset. Contains filtered measured current.
@@ -70,14 +72,15 @@ class AdaptiveBayesianInference(Process):
 			Determines the number of datapoints we use to fit the model. If it's 1, then we use all available datapoints.
 			If it's n > 1, we use every nth datapoint from the h5_main dataaset. 
 		kwargs		: (Optional) dictionary
-			Please see Process class for additional inputs
+			See Process class for additional inputs
 		"""
+
 		super(AdaptiveBayesianInference, self).__init__(h5_main, **kwargs)
 
-		# Name the process
+		## Name the process
 		self.process_name = 'Adaptive_Bayesian'
 
-		# Then set some variables...
+		## Set some constants and vectors for the Bayesian analysis.
 
 		# This determines how to parse down the data (i.e. used_data = actual_data[::parse_mod]).
 		self.parse_mod = parse_mod
@@ -95,35 +98,47 @@ class AdaptiveBayesianInference(Process):
 		# This line checks to make sure this data has not already been processed
 		self.duplicate_h5_groups, self.partial_h5_groups = self._check_for_duplicates()
 
-		
+		# This will hold the chunks of data from the h5_main dataset to be used in compute()
 		self.data = None
 
-		# A couple constants and vectors we will be using
+		# The data is currently ordered as a sine excitation wave. We want to feed the data into
+		# Bayesian inference algorithm as forward (-6 V to 6 V) and reverse (6 V to -6 V) sweeps,
+		# so we have to shift the data and split it in half.
 		self.full_V, self.shift_index, self.split_index = get_shift_and_split_indices(self.full_V)
 		self.dvdt = get_shifted_response(self.dvdt, self.shift_index)
+
+		# Grab the excitation amplitude if we don't have it
 		if V0 is None:
 			V0 = max(self.full_V)
+
+		# Grab the voltage points we will predict resistances for in the piecewise basis
 		self.M, self.dx, self.x = get_M_dx_x(V0=V0, M=M)
+
+		# Store a couple constants
 		self.V0 = V0
 		self.f = f
 		self.Ns = Ns
-		if self.verbose: print("data and variables set up")
-		# These will be the results from the processed chunks
+
+		if self.verbose: print("Bayesian inference parameters are set up")
+		
+		## Set up variables to store the results of the Bayesian algorithm
+
+		# These will be the results from each processed chunk to transfer to the datasets
 		self.R = None
 		self.R_sig = None
 		self.capacitance = None
 		self.i_recon = None
 		self.i_corrected = None
 	
-		# These are the actual databases
+		# These are the actual result databases
 		self.h5_R = None
 		self.h5_R_sig = None
 		self.h5_capacitance = None
 		self.h5_i_recon = None
 		self.h5_i_corrected = None
-		if self.verbose: print("empty results set up")
+		if self.verbose: print("Empty result variables are set up")
 
-		# Make full_V and num_pixels attributes
+		# Create a dictionary for the attributes of the results group
 		self.params_dict = dict()
 		self.params_dict["Ns"] = self.Ns
 		self.params_dict["num_pixels"] = self.h5_main[()].shape[0]
@@ -137,38 +152,62 @@ class AdaptiveBayesianInference(Process):
 		self.params_dict["dx"] = self.dx
 		self.params_dict["x"] = self.x
 		self.params_dict["parse_mod"] = self.parse_mod
-		if self.verbose: print("attributes set up")
+		if self.verbose: print("Results attributes dictionary created.")
 
-		#print("Ran init. self.params_dict is {}".format(self.params_dict))
+		# Yay we are done with setup!
+		if self.verbose: print("Finished initialization of AdaptiveBayesianInference class.")
 
 	def test(self, pix_ind=None):
 		"""
 		Tests the Bayesian inference on a single pixel (randomly chosen unless manually specified) worth of data.
-		Displays the resulting figure (resistances with variance, and reconstructed current)
-			before returning the same figure.
+		Returns the resulting figure that has the forward and reverse resistances and the corrected current.
+
 		Parameters
 		----------
-		pix_ind : int, optional. default = random
-			Index of the pixel whose data will be used for inference
+		pix_ind	: (Optional) integer
+			Index of the pixel whose data will be used to test Bayesian inference. If not provided,
+				a random pixel will be selected.
+
 		Returns
 		-------
-		fig, axes
+		pix_ind : integer
+			Index of the pixel whose data was used to test Bayesian inference.
+		figure	: pyplot.figure
+			Plot of the inferred resistances and corrected current that can be saved or displayed.
 		"""
+
+		# Only run this once if doing distributed processing
 		if self.mpi_rank > 0:
 			return
+
+		# Get a random pixel index if one is not provided
 		if pix_ind is None:
 			pix_ind = np.random.randint(1, high=self.h5_main.shape[0])
 
+		# Shift the data to get the forward and reverse sweeps
 		full_i_meas = get_shifted_response(self.h5_main[pix_ind, ::self.parse_mod], self.shift_index)
 
-		# Return from test function you built seperately (see gmode_utils.test_filter for example)
+		# Process the pixel and return the resulting figure.
 		return pix_ind, process_pixel(full_i_meas, self.full_V, self.split_index, self.M, self.dx, self.x, self.shift_index, self.f, self.V0, self.Ns, self.dvdt, pix_ind=pix_ind, graph=True, verbose=True)
 
-	def plotPixel(self, pix_ind=None):
-		if pix_ind is None:
-			return None
+	def plotPixel(self, pix_ind):
+		"""
+		Reads the resulting data of one of the pixels, plots out the forward and reverse resistances
+			and the corrected current, and returns the plot.
+
+		Parameters
+		----------
+		pix_ind	: integer
+			Index of the pixel whose data we want to plot.
+
+		Returns
+		-------
+		figure	: pyplot.figure
+			Plot of the inferred resistances and corrected current that can be saved or displayed.
+		"""
 
 		# Need x, R, R_sig, V, i_meas, i_recon, i_corrected to graph against .h5_spec_vals[()]
+		# Get these values from the results dataset that we presumably already created.
 		x = usid.USIDataset(self.h5_results_grp["Resistance"]).h5_spec_vals[()][0]
 		R = self.h5_results_grp["Resistance"][()][pix_ind, :]
 		R_sig = self.h5_results_grp["R_sig"][()][pix_ind, :]
@@ -177,21 +216,23 @@ class AdaptiveBayesianInference(Process):
 		i_recon = self.h5_results_grp["Reconstructed_Current"][()][pix_ind, :]
 		i_corrected = self.h5_results_grp["Corrected_Current"][()][pix_ind, :]
 
+		# Plot and return the plot of all these values.
 		return publicGetGraph(self.Ns, pix_ind, self.shift_index, self.split_index, x, R, R_sig, V, i_meas, i_recon, i_corrected)
 
 
 	def _create_results_datasets(self):
 		"""
-		Creates all the datasets necessary for holding all parameters + data.
+		Creates all the datasets necessary for holding the parameters and resulting inference data.
 		"""
+
+		## Create a group with the relevant attributes.
 
 		self.h5_results_grp = create_results_group(self.h5_main, self.process_name)
 
 		self.params_dict.update({'last_pixel': 0, 'algorithm': 'pycroscopy_AdaptiveBayesianInference'})
 
-		# Write in our full_V and num_pixels as attributes to this new group
+		# Write in the attributes using the dictionary created during initialization of the class.
 		write_simple_attrs(self.h5_results_grp, self.params_dict)
-
 		assert isinstance(self.h5_results_grp, h5py.Group)
 
 		# If we ended up parsing down the data, create new spectral datasets (i.e. smaller full_V's)
@@ -203,7 +244,7 @@ class AdaptiveBayesianInference(Process):
 			h5_spec_inds_new = self.h5_main.h5_spec_inds
 			h5_spec_vals_new = self.h5_main.h5_spec_vals
 
-		# Also make some new spectroscopic datasets for R and R_sig
+		# Make some new spectroscopic datasets for R and R_sig
 		h5_spec_inds_R, h5_spec_vals_R = write_ind_val_dsets(self.h5_results_grp, Dimension("Bias", "V", 2*self.M), is_spectral=True, base_name="Spectroscopic_R")
 		h5_spec_vals_R[()] = np.concatenate((self.x, self.x)).T
 
@@ -227,7 +268,6 @@ class AdaptiveBayesianInference(Process):
 												 h5_pos_vals=self.h5_main.h5_pos_vals,
 												 dtype=np.float64, aux_spec_prefix="Cap_Spec_")
 
-		# Not sure what units this should be so tentatively storing it as amps
 		self.h5_i_recon = write_main_dataset(self.h5_results_grp, (self.h5_main.shape[0], self.full_V.size), "Reconstructed_Current", "Current",
 											 "nA", None, None, dtype=np.float64,
 											 h5_pos_inds=self.h5_main.h5_pos_inds,
@@ -236,69 +276,68 @@ class AdaptiveBayesianInference(Process):
 											 h5_spec_vals=h5_spec_vals_new)
 		self.h5_i_corrected = create_empty_dataset(self.h5_i_recon, np.float64, "Corrected_Current")
 
-		'''
-		# Initialize our datasets
-		# Note, each pixel of the datasets will hold the forward and reverse sweeps concatenated together.
-		# R and R_sig are plotted against [x, -x], and i_recon and i_corrected are plotted against full_V.
-		self.h5_R = h5_results_grp.create_dataset("R", shape=(self.h5_main.shape[0], 2*self.M), dtype=np.float)
-		self.h5_R_sig = h5_results_grp.create_dataset("R_sig", shape=(self.h5_main.shape[0], 2*self.M), dtype=np.float)
-		self.h5_capacitance = h5_results_grp.create_dataset("capacitance", shape=(self.h5_main.shape[0], 2), dtype=np.float)
-		self.h5_i_recon = h5_results_grp.create_dataset("i_recon", shape=(self.h5_main.shape[0], self.full_V.size), dtype=np.float)
-		self.h5_i_corrected = h5_results_grp.create_dataset("i_corrected", shape=(self.h5_main.shape[0], self.full_V.size), dtype=np.float)
-		'''
-
-		if self.verbose: print("results datasets set up")
+		if self.verbose: print("Results datasets created.")
 		self.h5_main.file.flush()
 
 	def _get_existing_datasets(self):
 		"""
 		Extracts references to the existing datasets that hold the results
 		"""
-		# Do not worry to much about this now 
-		self.h5_R = self.h5_results_grp["R"]
+		
+		self.h5_R = self.h5_results_grp["Resistance"]
 		self.h5_R_sig = self.h5_results_grp["R_sig"]
-		self.h5_capacitance = self.h5_results_grp["capacitance"] * 1000 # convert from nF to pF
-		self.h5_i_recon = self.h5_results_grp["i_recon"]
-		self.h5_i_corrected = self.h5_results_grp["i_corrected"]
+		self.h5_capacitance = self.h5_results_grp["Capacitance"] * 1000 # convert from nF to pF
+		self.h5_i_recon = self.h5_results_grp["Reconstructed_Current"]
+		self.h5_i_corrected = self.h5_results_grp["Corrected_Current"]
 
 	def _write_results_chunk(self, pos_in_batch=None):
 		"""
 		Writes data chunks back to the file
+
+		Parameter
+		---------
+		pos_in_batch	: list-type object
+			Holds the list of pixel indices that we have just run Bayesian inference and
+				to store the results of.
 		"""
-		# Get access to the private variable:
+
+		# Get the pixels that we just ran the Bayesian inference on.
 		if pos_in_batch is None:
 			pos_in_batch = self._get_pixels_in_current_batch()
 
-		# Ex. if self.write_filtered:
-		#    self.h5_filtered[pos_in_batch, :] = self.filtered_data
+		# Write the results generated by _unit_computation() into the corresponding datasets
 		self.h5_R[pos_in_batch, :] = self.R
 		self.h5_R_sig[pos_in_batch, :] = self.R_sig
 		self.h5_capacitance[pos_in_batch, :] = self.capacitance
 		self.h5_i_recon[pos_in_batch, :] = self.i_recon
 		self.h5_i_corrected[pos_in_batch, :] = self.i_corrected
-		if self.verbose: print("results written back to file")
-		# Process class handles checkpointing.
+
+		if self.verbose: print("Results written back to file.")
+
+		# Note: the Process class handles checkpointing.
 
 	def _unit_computation(self, pos_in_batch=None, *args, **kwargs):
 		"""
-		Processing per chunk of the dataset
+		Processes a chunk of pixels (as given by pos_in_batch) in parallel and stores the results in local variables.
+
 		Parameters
 		----------
-		args : list
+		pos_in_batch	: list-type object
+			Holds the list of pixel indices that we want to run Bayesian inference on.
+		args 			: list
 			Not used
-		kwargs : dictionary
+		kwargs 			: dictionary
 			Not used
 		"""
 
+		# Grab the chunk of data that we want to process from the h5_main dataset
 		if pos_in_batch is not None:
 			self.data = self.h5_main[()][pos_in_batch, :]
 
-		# This is where you add the Matlab code you translated. You can add stuff to other Python files in processing, 
-		#like gmode_utils or comp_utils, depending on what your function does. Just add core code here.
+		# Shift and parse the data for each pixel so that we get our forward and reverse responses.
 		shifted_i_meas = parallel_compute(self.data[:, ::self.parse_mod], get_shifted_response, cores=self._cores, func_args=[self.shift_index])
 
-		#print("Shifted_i_meas is {}".format(shifted_i_meas))
-
+		# Run Bayesian inference on each pixel.
 		all_data = parallel_compute(np.array(shifted_i_meas), process_pixel, cores=self._cores,
 									func_args=[self.full_V, self.split_index, self.M, self.dx, self.x, self.shift_index, self.f, self.V0, self.Ns, self.dvdt])
 
@@ -310,8 +349,8 @@ class AdaptiveBayesianInference(Process):
 		self.capacitance = np.array([result[2] for result in all_data]).astype(np.float64)
 		self.i_recon = np.array([result[3].T[0] for result in all_data]).astype(np.float64)
 		self.i_corrected = np.array([result[4].T[0] for result in all_data]).astype(np.float64)
-		#print("capacitances are {} with shape {}".format(self.capacitance, self.capacitance.shape))
-		if self.verbose: print("chunk computed")
+
+		if self.verbose: print("Finished computing chunk.")
 
 
 
