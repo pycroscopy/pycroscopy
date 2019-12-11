b2c3ef2cb512db9ae1b1c3114d11cb493a196ea5
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-12-11 16:31:38 -0500

    Removing GuessMethods, FitMethods, Optimize from init

--- pycroscopy/analysis/__init__.py
+++ pycroscopy/analysis/__init__.py
@@ -23,14 +23,9 @@ from . import be_sho_fitter
 from .be_sho_fitter import BESHOfitter
 from . import be_loop_fitter
 from .be_loop_fitter import BELoopFitter
-from . import guess_methods
-from .guess_methods import GuessMethods
 from . import fitter
 from .fitter import Fitter
-from .optimize import Optimize
-from . import fit_methods
-from .fit_methods import *
 from .giv_bayesian import GIVBayesian
 
-__all__ = ['GuessMethods', 'Fitter', 'BESHOfitter', 'BELoopFitter', 'utils', 'Optimize', 'fit_methods', 'GIVBayesian']
+__all__ = ['Fitter', 'BESHOfitter', 'BELoopFitter', 'utils',  'GIVBayesian']
 __all__ += utils.__all__
