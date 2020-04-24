a9fa7420c8671ceaf22f0a138ccdc75ace7d9686
ramav87 <rama87@gmail.com>
2020-04-24 17:05:33 -0400

    No longer supporting python 2.7
    No longer supporting python 2.7

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Cython',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
