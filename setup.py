63c77f83556f99e5124d596c11682e7ecde6853e
ramav87 <rama87@gmail.com>
2020-04-24 15:43:08 -0400

    Removing numba as dependency
    Removing numba as dependency

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requirements = ['numpy>=1.13.0',
 
                 'numpy_groupies==0.9.7',  # New build of 0.9.8.4 appears to cause build problems
 
-                'numba',
+                 #'numba',We don't use numba
                 'pyUSID==0.0.5.1', #pyUSID is undergoing developments, so falling back for timebeing.
 
                 'xlrd>=1.0.0',
