927197fbc098dd5ccc66a3c7c257d90bc3392e66
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-07-02 10:43:43 -0400

    BEtter printing of UDVS table

--- pycroscopy/io/translators/df_utils/be_utils.py
+++ pycroscopy/io/translators/df_utils/be_utils.py
@@ -828,10 +828,10 @@ def createSpecVals(udvs_mat, spec_inds, bin_freqs, bin_wfm_type, parm_dict,
             print('\t' * 3 + '__FindSpecValIndices:')
             print('\t' * 4 + 'UDVS matrix of shape: {}'.format(udvs_mat.shape))
             print('\t' * 4 + 'spec_inds of shape: {}'.format(spec_inds.shape))
-            print('step_num\tdc_offset\tac_amp\twave_type\twave_mod\tin-field'
+            print('step_num\tdc_offset\tac_ampli\twave_type\twave_mod\tin-field'
                   '\tout-of-field')
             for ud_row in udvs_mat:
-                print('\t'.join(['{:04.2f}'.format(item) for item in ud_row]))
+                print('\t\t'.join(['{:04.2f}'.format(item) for item in ud_row]))
 
         #         Copy even step values of DC_offset into odd steps
         UDVS = np.copy(udvs_mat)
@@ -857,10 +857,10 @@ def createSpecVals(udvs_mat, spec_inds, bin_freqs, bin_wfm_type, parm_dict,
         if verbose:
             print('\t' * 4 + 'UDVS matrix after down-selecting rows: {}'
                              ''.format(UDVS.shape))
-            print('step_num\tdc_offset\tac_amp\twave_type\twave_mod\tin-field'
+            print('step_num\tdc_offset\tac_ampli\twave_type\twave_mod\tin-field'
                    '\tout-of-field')
-            for ud_row in udvs_mat:
-                print('\t'.join(['{:04.2f}'.format(item) for item in ud_row]))
+            for ud_row in UDVS:
+                print('\t\t'.join(['{:04.2f}'.format(item) for item in ud_row]))
 
         """
         Transpose UDVS for ease of looping later on and store the number of steps
