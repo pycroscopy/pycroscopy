f209deb828fa65f633c19ea86afaf42260f97dff
ssomnath <14300780+ssomnath@users.noreply.github.com>
2021-02-12 15:41:31 -0500

    Removing translators that are either unused or available as Readers

--- pycroscopy/io/translators/__init__.py
+++ pycroscopy/io/translators/__init__.py
@@ -7,30 +7,20 @@ Created on Tue Jan 05 07:55:56 2016
 
 @author: Suhas Somnath, Chris Smith
 """
-from .igor_ibw import IgorIBWTranslator
-from .ndata import NDataTranslator
-from .oneview import OneViewTranslator
-from .image_stack import PtychographyTranslator
-from .image_stack import ImageStackTranslator
-from .time_series import MovieTranslator
+from .AR_hdf5 import ARhdf5
 from .bruker_afm import BrukerAFMTranslator
 from .nanonis import NanonisTranslator, NanonisTranslatorCorrect
-from .image import ImageTranslator
 from .pifm import PiFMTranslator
-from .gwyddion import GwyddionTranslator
-from .omicron_asc import AscTranslator
 
-__all__ = ['MovieTranslator', 'IgorIBWTranslator',
-           'OneViewTranslator', 'NDataTranslator', 'PtychographyTranslator',
-           'BrukerAFMTranslator', 'ImageTranslator',
-           'PiFMTranslator', 'NanonisTranslator', 'GwyddionTranslator', 'AscTranslator']
+__all__ = ['BrukerAFMTranslator', 'PiFMTranslator', 'NanonisTranslator',
+           'ARhdf5']
 
-afm_translators = [IgorIBWTranslator, PiFMTranslator, BrukerAFMTranslator, GwyddionTranslator]
+afm_translators = [PiFMTranslator, BrukerAFMTranslator, AR_hdf5]
 
-stm_translators = [NanonisTranslatorCorrect, AscTranslator]
+stm_translators = [NanonisTranslatorCorrect]
 
-stem_translators = [NDataTranslator, OneViewTranslator]
+stem_translators = []
 
-misc_translators = [ImageStackTranslator, MovieTranslator, ImageTranslator]
+misc_translators = []
 
 all_translators = afm_translators + stm_translators + stem_translators + misc_translators
