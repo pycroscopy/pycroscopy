8f4440e7e156413f1380a594a540d5a52380ed70
ziatdinovmax <ziatdinovmax@gmail.com>
2021-03-07 20:17:58 +0000

    Update imports

--- pycroscopy/learn/dl/__init__.py
+++ pycroscopy/learn/dl/__init__.py
@@ -1,3 +1,5 @@
-from .nnblocks import ConvBlock, UpsampleBlock
+from .nnblocks import (ConvBlock, UpsampleBlock, features_to_latent,
+                       latent_to_features)
 
-__all__ = ['ConvBlock', 'UpsampleBlock']
+__all__ = ['ConvBlock', 'UpsampleBlock', 'latent_to_features',
+           'features_to_latent']
