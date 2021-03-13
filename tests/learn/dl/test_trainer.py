99984f9e70a71c2d5d3e1376e39ee93880e633a0
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-03-13 04:25:23 +0000

    Rename run() to fit() Update normalization for losses Specify number of epochs in fit() instead of __init__()

--- tests/learn/dl/test_trainer.py
+++ tests/learn/dl/test_trainer.py
@@ -20,15 +20,16 @@ def assert_weights_equal(m1, m2):
 
 def test_trainer():
     # Initialize a model
-    in_dim = (1, 16, 16)
-    model = models.AutoEncoder(in_dim, layers_per_block=[1, 2])
+    in_dim = (1, 8, 8)
+    model = models.AutoEncoder(
+        in_dim, layers_per_block=[1, 2], nfilters=2)
     weights_before = model.state_dict()
     # Create dummy train set
-    X_train = torch.randn(60, 1, 16, 16)
+    X_train = torch.randn(10, 1, 8, 8)
     # Initialize trainer
-    t = Trainer(model, X_train, X_train, num_epochs=2)
+    t = Trainer(model, X_train, X_train, batch_size=4)
     # train and compare model params before and after
-    t.run()
+    t.fit(num_epochs=2)
     weights_after = model.state_dict()
     assert_(not assert_weights_equal(weights_before, weights_after))
 
@@ -36,20 +37,22 @@ def test_trainer():
 def test_trainer_determenism():
     # Create dummy train set
     torch.manual_seed(0)
-    X_train = torch.randn(60, 1, 16, 16)
+    X_train = torch.randn(10, 1, 8, 8)
     # Initialize a model
-    in_dim = (1, 16, 16)
+    in_dim = (1, 8, 8)
     model1 = models.AutoEncoder(
-        in_dim, layers_per_block=[1, 2], upsampling_mode="nearest")
+        in_dim, layers_per_block=[1, 2], nfilters=2,
+        upsampling_mode="nearest")
     # Initialize trainer
-    t = Trainer(model1, X_train, X_train, num_epochs=5)
+    t = Trainer(model1, X_train, X_train, batch_size=4)
     # train
-    t.run()
+    t.fit(num_epochs=5)
     # Reininitiaize model and train again
     torch.manual_seed(0)
-    X_train = torch.randn(60, 1, 16, 16)
+    X_train = torch.randn(10, 1, 8, 8)
     model2 = models.AutoEncoder(
-        in_dim, layers_per_block=[1, 2], upsampling_mode="nearest")
-    t = Trainer(model2, X_train, X_train, num_epochs=5)
-    t.run()
+        in_dim, layers_per_block=[1, 2], nfilters=2,
+        upsampling_mode="nearest")
+    t = Trainer(model2, X_train, X_train, batch_size=4)
+    t.fit(num_epochs=5)
     assert_(assert_weights_equal(model1.state_dict(), model2.state_dict()))
