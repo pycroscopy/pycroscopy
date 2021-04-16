d85ba69472656aab7b46ce1b161f4b2e11253829
ssomnath <14300780+ssomnath@users.noreply.github.com>
2021-04-16 16:44:20 -0400

    Added warning when importing package

--- pycroscopy/__init__.py
+++ pycroscopy/__init__.py
@@ -7,6 +7,7 @@ Submodules
 .. autosummary::
     :toctree: _autosummary
 """
+from warnings import warn
 from .io import translators
 from .io.translators import ImageTranslator  # Use pycroscopy version of ImageTranslator rather than pyUSID's
 from . import analysis
@@ -15,3 +16,6 @@ from . import viz
 
 from .__version__ import version as __version__
 from .__version__ import time as __time__
+
+warn('The subsequent version of pycroscopy will not be backwards compatible '
+     'since it will be substantially overhauled', FutureWarning)
