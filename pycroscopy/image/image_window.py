94057393a1775a77451c2277d82b0f48ef74e570
RamaKVasudevan <rama87@gmail.com>
2021-04-23 18:14:41 -0400

    Image windowing - added ability to tke FFT and also add zoom, rescaling, and filtering to individual windows.

--- pycroscopy/image/image_window.py
+++ pycroscopy/image/image_window.py
@@ -3,7 +3,9 @@
 
 import numpy as np
 from scipy import fftpack
-from scipy import ndimage
+from scipy.ndimage import zoom
+from scipy.signal import hanning, blackman
+from skimage.transform import rescale
 import sidpy
 
 class ImageWindowing:
@@ -11,6 +13,10 @@ class ImageWindowing:
     def __init__(self, parms_dict):
         '''Sliding Window Class.
        This class will generate windows from 2D sidpy dataset objects
+
+       #TODO: add the docstrings for all the options for the windowing
+
+
        '''
 
         self.window_step_x = parms_dict['window_step_x']
@@ -18,37 +24,83 @@ class ImageWindowing:
         self.window_size_x = parms_dict['window_size_x']
         self.window_size_y = parms_dict['window_size_y']
 
-        #self.interpol_factor = parms_dict['interpolation_factor']
-        #self.zoom_factor = parms_dict['zoom_factor']
-        #self.hamming_filter = parms_dict['hamming_filter']
+        if 'mode' in parms_dict.keys():
+            if parms_dict['mode'] not in ['image', 'fft']:
+                raise ValueError("Parameters dictionary field 'mode' must be one of 'image' or 'fft'."
+                                 "Try again with one of these two options.")
+            else:
+                self.mode = parms_dict['mode']
+        else:
+            self.mode = 'image'
+
+        if 'interpol_factor' in parms_dict.keys(): self.interpol_factor = parms_dict['interpol_factor']
+        else: self.interpol_factor = 1
+
+        if 'zoom_factor' in parms_dict.keys(): self.zoom_factor = parms_dict['zoom_factor']
+        else: self.zoom_factor = 1
+
+        # Based on the zoom and interpolation factors we need to figure out the final size of the window
+        self.window_size_final_x, self.window_size_final_y = self._get_window_size()
+        #Setup the filter for the window
+        self.filter = 'None'
+        self.filter_mat = np.ones((self.window_size_final_x, self.window_size_final_y))
+
+        if self.mode=='fft':
+            #load FFT options
+            if 'filter' in parms_dict.keys():
+                if parms_dict['filter'] not in ['blackman', 'hanning']:
+                    raise ValueError("Parameter 'filter' must be one of 'hanning', 'blackman'")
+                else:
+                    self.filter = parms_dict['filter']
+                    if self.filter=='hanning':
+                        filter_x = hanning(self.window_size_final_x)
+                        filter_y = hanning(self.window_size_final_y)
+                        self.filter_mat = np.sqrt(np.outer(filter_x,filter_y))
+                    elif self.filter=='blackman':
+                        filter_x = blackman(self.window_size_final_x)
+                        filter_y = blackman(self.window_size_final_y)
+                        self.filter_mat = np.sqrt(np.outer(filter_x,filter_y))
+
         return
 
-    def MakeWindows(self, dataset, dim_slice = None):
+    def _get_window_size(self):
+        '''
+        Computes window size based on zoom and interpolation factors
         '''
 
-        image should be a sidpy dataset object
-        We will take the image to be the first two spatial dimensions,
-        unless dimensions are specified
+        image_test = np.random.uniform(size=(self.window_size_x, self.window_size_y))
 
-        Inputs:
-            - dataset (sidpy.Dataset object of the image to be windowed)
-            - dim_slice (List) (Optional). list of integers of the slices over which the
-            image windowing should take place. This should be of length number of dimensions of
-            the dataset minus two.
+        image_zoomed = zoom(image_test, self.zoom_factor)
 
-        Returns:
-            - windowed_dataset (sidpy.Dataset) object with windows created as per
-            the parameters passed to the ImageWindowing class.
+        #interpolate it
+        zoomed_interpolated = rescale(image_zoomed, self.interpol_factor)
 
+        return zoomed_interpolated.shape[0],zoomed_interpolated.shape[1]
+
+    def MakeWindows(self, dataset, dim_slice=None):
         '''
+            Image should be a sidpy dataset object
+            We will take the image to be the first two spatial dimensions,
+            unless dimensions are specified
+
+            Inputs:
+                - dataset (sidpy.Dataset object of the image to be windowed)
+                - dim_slice (List) (Optional). list of integers of the slices over which the
+                image windowing should take place. This should be of length number of dimensions of
+                the dataset minus two.
+
+            Returns:
+                - windowed_dataset (sidpy.Dataset) object with windows created as per
+                the parameters passed to the ImageWindowing class.
+
+            '''
 
         # This is the windowing function. Will generate the windows (but not the FFT)
         num_dimensions = dataset.ndim
-
         if dim_slice is None:
-            if num_dimensions>2:
+            if num_dimensions > 2:
                 raise ValueError('You have specified windowing on a sidpy dataset '
-                      'with more than 2 dimensions without specifying slices')
+                                 'with more than 2 dimensions without specifying slices')
             else:
                 image_source = dataset[:]
         elif dim_slice is not None:
@@ -58,79 +110,100 @@ class ImageWindowing:
                 if axis.dimension_type == sidpy.DimensionType.SPATIAL:
                     image_dims.append(dim)
             all_dims = np.arange(0, num_dimensions)
-            image_source = dataset[image_dims]
+            slice_list = []
+            for k in range(num_dimensions):
+                if k in image_dims:
+                    slice_list.append(slice(None, dataset.shape[k], 1))
+                else:
+                    slice_list.append(dim_slice)
+            image_source = dataset[tuple(slice_list)]
 
         image_shape = image_source.shape
 
         window_step = [self.window_step_x, self.window_step_y]
         window_size = [self.window_size_x, self.window_size_y]
+        window_size_final = [self.window_size_final_x, self.window_size_final_y]
 
         dim_vec = []
-        for i in range(num_dimensions):
+        for i in range(2):
+            #print(image_shape[i], window_size[i], window_step[i])
             dim_vec.append(np.arange(0, image_shape[i] - window_size[i], window_step[i]))
+        #print("dim vec is {}".format(dim_vec))
 
         _, pos_vec = self.build_ind_val_matrices(dim_vec)
-        pca_mat = np.zeros(shape=(pos_vec.shape[0], np.prod(window_size)), dtype=np.complex64)
+        #print("Pos vec is {}".format(pos_vec))
+
+        pca_mat = np.zeros(shape=(pos_vec.shape[0], np.prod(window_size_final)), dtype=np.complex64)
         pos_vec = np.int32(pos_vec)
 
         for ind, pos in enumerate(pos_vec):
             start_stop = [slice(x, x + y, 1) for x, y in zip(pos, window_size)]
-            full_slice = image[start_stop]
+            full_slice = image_source[tuple(start_stop)]
+            full_slice = self._return_win_image_processed(full_slice)
+
             pca_mat[ind] = full_slice.flatten()
 
         self.pos_vec = pos_vec
+        # Get the positions and make them dimensions
+        new_x_vals = np.unique(pos_vec[:, 0])
+        new_y_vals = np.unique(pos_vec[:, 1])
 
-        return pca_mat.reshape(-1, self.window_size_x, self.window_size_y)
+        windows_reshaped = pca_mat.reshape(len(new_x_vals), len(new_y_vals),
+                                           self.window_size_final_x, self.window_size_final_y)
 
-    def ApplyFilter(self, imgsrc, filter_type = 'Hamming'):
-        # Applies a Hamming window to the input imgsec,
-        # returns the window after filter applied.
-        bw2d = np.outer(np.hamming(self.window_size_x), np.ones(self.window_size_y))
-        bw2d = np.sqrt(bw2d * bw2d.T)
-        imgsrc *= bw2d
-        return imgsrc
+        # Now we need to return the sidpy dataset object
 
-    def zoom_interpol(self, FFT_image):
+        # Make a sidpy dataset
+        data_set = sidpy.Dataset.from_array(windows_reshaped,
+                                            name='Current_spectral_map_Windowed')
 
-        # Accepts an image, returns zoomed image
-        zoom_size = (FFT_image.shape[0] / self.zoom_factor) / 2
+        # Set the data type
+        data_set.data_type = 'IMAGE_STACK'
 
-        if np.mod(FFT_image.shape[0] / self.zoom_factor, 2) == 0:
-            F2_zoomed = FFT_image[int(self.window_size_x / 2 - zoom_size):int(self.window_size_x / 2 + zoom_size),
-                        int(self.window_size_y / 2 - zoom_size):int(self.window_size_y / 2 + zoom_size)]
+        # Add quantity and units
+        data_set.units = dataset.units
+        data_set.quantity = dataset.quantity
+
+        # Add dimension info
+        # TODO: Fix the quantities and stuff here
+
+        z_dimx = np.arange(0, data_set.shape[2])
+        z_dimy = np.arange(0, data_set.shape[3])
+
+        data_set.set_dimension(0, sidpy.Dimension(new_x_vals,
+                                                  name='x',
+                                                  units='m', quantity='x',
+                                                  dimension_type='spatial'))
+        data_set.set_dimension(1, sidpy.Dimension(new_y_vals,
+                                                  name='y',
+                                                  units='m', quantity='y',
+                                                  dimension_type='spatial'))
+
+        data_set.set_dimension(2, sidpy.Dimension(z_dimx,
+                                                  name='WindowX',
+                                                  units='m', quantity='kx',
+                                                  dimension_type='spectral'))
+
+        data_set.set_dimension(3, sidpy.Dimension(z_dimy,
+                                                  name='WindowY',
+                                                  units='m', quantity='ky',
+                                                  dimension_type='spectral'))
+
+        # append metadata
+        # data_set.metadata = parms_dict_expt
+        return data_set
+
+    def _return_win_image_processed(self, img_window):
+        #Real image slice, returns it back with image processed
+        if self.zoom_factor==1 and self.interpol_factor==1 and self.filter == 'None':
+            #simply skip this function if there is no zooming, interpolation to be done.
+            return img_window
         else:
-            F2_zoomed = FFT_image[int(self.window_size_x / 2 - zoom_size):int(self.window_size_x / 2 + 1 + zoom_size),
-                        int(self.window_size_y / 2 - zoom_size):int(self.window_size_y / 2 + 1 + zoom_size)]
-
-        zoomed_interpolated_image = ndimage.zoom(F2_zoomed, self.interpol_factor)
-        self.window_size_final_x = zoomed_interpolated_image.shape[0]
-        self.window_size_final_y = zoomed_interpolated_image.shape[1]
-        return ndimage.zoom(F2_zoomed, self.interpol_factor)
-
-    def Do_Sliding_FFT(self, windows):
-
-        # Carries out the FFT on the windows
-        FFT_mat4 = []
-
-        for i in range(windows.shape[0]):
-            img_window = windows[i, :, :]
-
-            if self.hamming_filter:  # Apply filter if requested
-                img_window_filtered = self.ApplyHamming(np.copy(img_window))
-            else:
-                img_window_filtered = (np.copy(img_window))
-
-            # Take the fourier transform of the image.
-            F1 = fftpack.fft2((img_window_filtered))
-
-            # Now shift so that low spatial frequencies are in the center.
-            F2 = (fftpack.fftshift((F1)))
-
-            final_FFT = self.zoom_interpol(np.abs(F2))
-
-            FFT_mat4.append(final_FFT)
-
-        return np.array(FFT_mat4)
+            img_window = zoom(img_window, self.zoom_factor) #Zoom it
+            img_window = rescale(img_window, self.interpol_factor) #Rescale
+            img_window *= self.filter_mat  # Apply filter
+            if self.mode == 'fft': img_window = np.fft.fftshift(np.fft.fft2(img_window))
+        return img_window
 
     def build_ind_val_matrices(self, unit_values):
         """
