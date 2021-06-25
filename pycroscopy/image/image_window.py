Image windowing addition - added mode for 'abs' and 'phase' and fixed quantities and units and scales to be consistent with the windowing. Also added metadata to the processed windows. Finally, added a 'reconstruct image' method, and outline for tests

pycroscopy/image/image_window.py
@@ -136,6 +136,7 @@ class ImageWindowing:
                                  'with more than 2 dimensions without specifying slices')
             else:
                 image_source = dataset[:]
+                image_dims = [0,1]
         elif dim_slice is not None:
             """Get all spatial dimensions"""
             image_dims = []
@@ -151,9 +152,9 @@ class ImageWindowing:
                     slice_list.append(dim_slice)
             image_source = dataset[tuple(slice_list)]
 
-        image_shape = image_source.shape
+        self.image_shape = image_source.shape
         if self.verbose:
-            print('Full image shape is {}'.format(image_shape))
+            print('Full image shape is {}'.format(self.image_shape))
 
         window_step = [self.window_step_x, self.window_step_y]
         window_size = [self.window_size_x, self.window_size_y]
@@ -161,7 +162,7 @@ class ImageWindowing:
 
         dim_vec = []
         for i in range(2):
-            dim_vec.append(np.arange(0, image_shape[i] - window_size[i], window_step[i]))
+            dim_vec.append(np.append(np.arange(0, self.image_shape[i] - window_size[i], window_step[i]), self.image_shape[i] - window_size[i]))
 
         _, pos_vec = self.build_ind_val_matrices(dim_vec)
         if self.verbose:
@@ -211,8 +212,8 @@ class ImageWindowing:
 
         # Add dimension info
 
-        window_size_fraction_x = window_size[0]/image_shape[0]
-        window_size_fraction_y = window_size[1] / image_shape[1]
+        window_size_fraction_x = window_size[0]/self.image_shape[0]
+        window_size_fraction_y = window_size[1] / self.image_shape[1]
 
         window_extent_x = (dataset._axes[image_dims[0]].values.max() -
                            dataset._axes[image_dims[0]].values.min())*window_size_fraction_x
@@ -271,6 +272,72 @@ class ImageWindowing:
         #given two dictionaries, merge them into one
         merged_dict = {**dict1, **dict2}
         return merged_dict
+
+    def build_clean_image(self, windows_2d):
+
+        #TODO: Output a sidpy dataset instead of the numpy array
+
+        """
+        Reconstructs the cleaned image from the windowed dataset
+        Parameters
+        ----------
+        windows_2d: windows dataset, can be sidpy dataset or numpy array, made by the MakeWindows() method
+                    Typically these windows are modified, e.g. cleaned by SVD, and then passed to this method
+                    to regenerate the image
+
+        Returns
+        -------
+        h5_clean : numpy arr
+            The cleaned image
+        """
+
+        win_x = self.window_size_final_x
+        win_y = self.window_size_final_y
+        win_step_x = self.window_step_x
+        win_step_y = self.window_step_y
+        im_x, im_y = self.image_shape[0], self.image_shape[1]
+
+        '''
+        Calculate the steps taken to create original windows
+        '''
+        x_steps = np.arange(0, im_x - win_x + 1, win_step_x)
+        y_steps = np.arange(0, im_y - win_y + 1, win_step_y)
+
+        '''
+        Initialize arrays to hold summed windows and counts for each position
+        '''
+        counts = np.zeros([im_x, im_y], np.uint8)
+        accum = np.zeros([im_x, im_y], np.float32)
+
+        nx = len(x_steps)
+        ny = len(y_steps)
+        n_wins = nx * ny
+
+        '''
+        Create slice object from the positions
+        '''
+        win_slices = [[slice(x, x + win_x), slice(y, y + win_y)] for x, y in np.array([np.tile(x_steps, nx),
+                                                                                       np.repeat(y_steps, ny)]).T]
+
+        '''
+        Loop over all windows.  Increment counts for window positions and 
+        add current window to total.
+        '''
+        ones = np.ones([win_x, win_y], dtype=counts.dtype)
+        for islice, this_slice in enumerate(win_slices):
+            selected = islice % np.rint(n_wins / 10) == 0
+            if selected:
+                per_done = np.rint(100 * islice / n_wins)
+                print('Reconstructing Image...{}% -- step # {}'.format(per_done, islice))
+            counts[this_slice] += ones
+
+            accum[this_slice] += windows_2d[islice].reshape(win_x, win_y)
+
+        clean_image = accum / counts
+
+        clean_image[np.isnan(clean_image)] = 0
+
+        return clean_image
 
     def build_ind_val_matrices(self, unit_values):
         """