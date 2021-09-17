e837f082cad36e840c61811ab03ca0652f3ba489
Rama Vasudevan <vasudevanrk@ornl.gov>
2021-09-17 16:56:08 -0400

    added file

--- pycroscopy/stats/__init__.py
+++ pycroscopy/stats/__init__.py
@@ -1,11 +1,12 @@
 """
 Statistics Library
 
-Could contain
+Should eventually contain
 - gaussian process regression
 - compressed sensing for image reconstruction
 - other relevant statistics tools, e.g. bayesian inference tools
 - tree sturcture objects for e.g. functional fit propagation
+- local crystallography
 
 Submodules
 ----------
