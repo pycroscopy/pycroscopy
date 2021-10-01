9df27666a1229853f404119bbc6ff228da1869ce
Yongtao Liu <liuy3@ornl.gov>
2021-10-01 16:24:14 -0400

    add svd, pca, nmf, ica factorization, and plot results. current version only allows to plot 2D, 3D spectral_image, and 3D image_stack data.

--- pycroscopy/learn/ml/matrix_factor.py
+++ pycroscopy/learn/ml/matrix_factor.py
@@ -5,7 +5,8 @@
 import sidpy
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.decomposition import NMF
+from sklearn import decomposition
+from matplotlib import gridspec
 
 class MatrixFactor():
     def __init__(self, data, method = 'svd', n_components = 5, normalize = False) -> None:
@@ -15,6 +16,8 @@ class MatrixFactor():
             - None
         """
         self.data = data
+        self.shape = data.shape()
+        self.data_type = data.data_type
         self.allowed_methods = ['svd', 'nmf', 'ica', 'nfindr', 'kernelpca'] #Might want to add others...
         assert method in self._allowed_methods, "Method must be one of {}".format(self._allowed_methods)
         self.method = method
@@ -39,23 +42,36 @@ class MatrixFactor():
         """
         
         if self.method == 'svd':
-            u,s,v = np.linalg.svd(self.data_2d)
-            components = s*v.T #check...
-            abundances = u
-            #components = ...
-            #abundances = ...
+            print("Performing SVD fitting")
+            svd = decomposition.TruncatedSVD(n_components=self.n_components)
+            abundances = svd.fit_transform(self.data_2d)
+            components = svd.components_
+         
         elif self.method =='nmf':
-            # code goes here...
-            nmf = NMF(n_components = self.num_components)
-            nmf.fit_transform(self.data_2d[:])
+            print("Performing NMF fitting")
+            nmf = decomposition.NMF(n_components = self.n_components)
+            abundances = nmf.fit_transform(self.data_2d)
             components = nmf.components_
+
+        elif self.method == 'pca':
+            print("Performing PCA fitting")
+            pca = decomposition.PCA(n_components=self.n_components)
+            abundances = pca.fit_transform(self.data_2d)
+            components = pca.components_
+
+        elif self.method == 'ica':
+            print("Performing ICA fitting")
+            ica = decomposition.FastICA(n_components=self.n_components)
+            abundances = ica.fit_transform(self.data_2d)
+            components = ica.components_
             
 
-        #Then we want to return components, abundances, and explained variances if available.
-        # We should return a list of sidpy dataset objects
+        #return components and abundances.
         self.components = components
         self.abundances = abundances
         self.results_computed  = True
+
+        print("Fitting completed, run plot_results to look at the results")
 
         return components, abundances
 
@@ -66,14 +82,54 @@ class MatrixFactor():
         self._plot_components(self.components)
 
     def _plot_abundances(self, abundances)-> plt.figure:
-        # We may need to think about this a little it
-        # Note that we have some viz in sidpy already. We can try to leverage.
-        fig, axes = plt.subplots()
+        print('Abundances')
+        gs = gridspec.GridSpec(int(self.n_components//3)+1, 3)
+        fig = plt.figure(figsize = (4*(int(self.n_components//3)+1), 4*(4//1.5)))
+        if len(self.shape) == 2:
+            for i in range(self.n_components):
+                ax = fig.add_subplot(gs[i])
+                ax.plot(self.abundances[:, i])
+                ax.set_title('Component ' + str(i + 1))
+            plt.show()
+        elif self.data_type == "SPECTRAL_IMAGE":
+            d1, d2, _ = self.shape
+            for i in range(self.n_components):
+                ax = fig.add_subplot(gs[i])
+                ax.imshow(self.abundances[:, i].reshape(d1, d2))
+                ax.set_title('Component ' + str(i + 1))
+            plt.show()
+        elif self.data_type == "IMAGE_STACK":
+            for i in range(self.n_components):
+                ax = fig.add_subplot(gs[i])
+                ax.plot(self.abundances[:, i])
+                ax.set_title('Component ' + str(i + 1))
+            plt.show()
 
         return fig
 
     def _plot_components(self, abundances)-> plt.figure:
-        # We may need to think about this a little it
-        fig, axes = plt.subplots()
+        print('Components')
+
+        gs = gridspec.GridSpec(int(self.n_components//3)+1, 3)
+        fig = plt.figure(figsize = (4*(int(self.n_components//3)+1), 4*(4//1.5))))
+        if len(self.shape) == 2:   
+            for i in range(self.n_components):
+                ax = fig.add_subplot(gs[i])
+                ax.plot(components[i])
+                ax.set_title('Component ' + str(i + 1))
+            plt.show()
+        elif self.data_type == "SPECTRAL_IMAGE":
+            for i in range(self.n_components):
+                ax = fig.add_subplot(gs[i])
+                ax.plot(components[i])
+                ax.set_title('Component ' + str(i + 1))
+            plt.show()
+        elif self.data_type == "IMAGE_STACK":
+            d1, d2, _ = self.shape 
+            for i in range(self.n_components):
+                ax = fig.add_subplot(gs[i])
+                ax.plot(components[i].reshape(d1, d2))
+                ax.set_title('Component ' + str(i + 1))
+            plt.show()
         
         return fig
