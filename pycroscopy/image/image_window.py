02a5540322ba0d11924635d1d45b24d7e4159092
Rama Vasudevan <vasudevanrk@ornl.gov>
2023-02-03 15:44:27 -0500

    FIxed bugs in imagewindowing  and updated tests.

--- pycroscopy/image/image_window.py
+++ pycroscopy/image/image_window.py
@@ -216,11 +216,15 @@ class ImageWindowing:
         window_step = [self.window_step_x, self.window_step_y]
         window_size = [self.window_size_x, self.window_size_y]
         window_size_final = [self.window_size_final_x, self.window_size_final_y]
-
-        assert self.image_shape[0]%self.window_step_x ==0, "Image shape {} and window step along x ({}) are not divisible \
-        without remainder, change your image shape or window step".format(self.image_shape[0], self.window_step_x)
-        assert self.image_shape[1]%self.window_step_y ==0, "Image shape {} and window step along y ({}) are not divisible \
-        without remainder, change your image shape or window step".format(self.image_shape[1], self.window_step_y)
+        division_factor_x = self.window_size_x - self.window_step_x
+        division_factor_y = self.window_size_y - self.window_step_y
+        if self.window_size_x == self.window_step_x: division_factor_x = self.window_size_x
+        if self.window_size_y == self.window_step_y: division_factor_y = self.window_size_y
+        
+        assert self.image_shape[0]%division_factor_y ==0, "Image shape along y is {} but window size is {}, window step is ({}) are not divisible \
+        without remainder, change your window size or window step".format(self.image_shape[0], self.window_size_x, self.window_step_x)
+        assert self.image_shape[1]%division_factor_x==0, "Image shape along x is {} but window size is {}, and window step is ({}) are not divisible \
+        without remainder, change your window size or window step".format(self.image_shape[1], self.window_size_y, self.window_step_y)
 
         dim_vec = []
         for i in range(2):
