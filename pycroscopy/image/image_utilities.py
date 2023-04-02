d593865ed6827f84b2075ad8dae61b7d00761cbc
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-04-02 10:08:50 +0200

    Added detailed docstrings, input format checking
    + more error handling, and removed pixel size hardcoding to improve function's robustness and user-friendliness.

--- pycroscopy/image/image_utilities.py
+++ pycroscopy/image/image_utilities.py
@@ -2,32 +2,48 @@ import numpy as np
 import sidpy
 
 
-def crop_image(dataset, corners):
+def crop_image(dataset: sidpy.Dataset, corners: np.ndarray) -> sidpy.Dataset:
     """
-    crops an image according to the corners given (in the format of matplotlib.widget.RectangleSelector)
-    
+    Crops an image according to the corners given in the format of matplotlib.widget.RectangleSelector.
+
     Parameters
     ----------
     dataset: sidpy.Dataset
-        image to be selected from
-    
+        An instance of sidpy.Dataset representing the image to be cropped.
+    corners: np.ndarray
+        A 1D array of length 4 containing the corners of the rectangular region to be cropped.
+        The order of the corners should be (x1, y1, x2, y2).
+
+    Returns
+    -------
+    sidpy.Dataset
+        A new instance of sidpy.Dataset representing the cropped image.
+
+    Raises
+    ------
+    ValueError
+        If dataset is not an instance of sidpy.Dataset or if dataset is not an image dataset.
+        If corners parameter is not of correct shape or size.
+
     """
     if not isinstance(dataset, sidpy.Dataset):
-        raise ValueError('Need a sidpy dataset')
+        raise ValueError('Input dataset is not an instance of sidpy.Dataset')
     if not dataset.data_type.name == 'IMAGE':
-        raise ValueError('Only Images are supported at this point')
-
-    corners = np.array(corners)
-    if corners.ndim <2 or corners.flatten().shape[0] < 4:
-        raise ValueError('Not enough corners given')
+        raise ValueError('Only image datasets are supported at this point')
     
-    pixel_size =  np.array([[dataset.x[1]-dataset.x[0]]*corners.shape[1], [dataset.y[1]-dataset.y[0]]*corners.shape[1]])
+    if corners.shape != (4,):
+        raise ValueError('Input corners parameter should have shape (4,) but got shape {0}'.format(corners.shape))
+    if corners[2]-corners[0] <= 0 or corners[3]-corners[1] <= 0:
+        raise ValueError('Invalid input corners parameter')
+    
+    pixel_size = np.array([dataset.x[1]-dataset.x[0], dataset.y[1]-dataset.y[0]])
     corners /= pixel_size
     
-    selection =np.stack([np.min(corners, axis=1)+0.5,np.max(corners, axis=1)+0.5]).astype(int)
+    selection = np.stack([np.min(corners[:2])+0.5, np.max(corners[2:])+0.5]).astype(int)
 
-    selected_dset = dataset.like_data(dataset[selection[0,0]:selection[1,0],selection[0,1]:selection[1,1]])
-    selected_dset.title = 'cropped_' + dataset.title
-    selected_dset.source = dataset.title
-    selected_dset.metadata ={'crop_dimension': selection, 'original_dimensions': dataset.shape}
-    return selected_dset
+    cropped_dset = dataset.like_data(dataset[selection[0, 0]:selection[1, 0], selection[0, 1]:selection[1, 1]])
+    cropped_dset.title = 'cropped_' + dataset.title
+    cropped_dset.source = dataset.title
+    cropped_dset.metadata = {'crop_dimension': selection, 'original_dimensions': dataset.shape}
+    
+    return cropped_dset
