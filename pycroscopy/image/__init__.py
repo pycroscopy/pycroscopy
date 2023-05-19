d7e2af8c2fdb2e8bd2ca0a0a7308c7c04030cc7e
Rama Vasudevan <vasudevanrk@ornl.gov>
2023-05-19 17:11:05 -0400

    line

--- pycroscopy/image/__init__.py
+++ pycroscopy/image/__init__.py
@@ -15,9 +15,8 @@ Submodules
     :toctree: _autosummary
 
 """
-
+ 
 from .image_window import ImageWindowing
-
 from .image_utilities import crop_image, flatten_image
 from .image_clean import decon_lr, clean_svd
 from .image_atoms import find_atoms, atom_refine
